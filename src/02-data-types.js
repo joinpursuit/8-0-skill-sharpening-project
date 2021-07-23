9fcee0b02112fe9effa2492bee8c10bf3c311d5d
Samantha Gosda <gosdasam@gmail.com>
2021-07-23 14:57:52 -0400

    completed convertToWhisperText func

--- src/02-data-types.js
+++ src/02-data-types.js
@@ -71,7 +71,9 @@ function convertToShoutingText(text) {
  *  convertToWhisperText("Hello There");
  *  //> "hello there"
  */
-function convertToWhisperText(text) {}
+function convertToWhisperText(text) {
+  return text.toLowerCase();
+}
 
 /**
  * checkIfCharacterIsInString()
