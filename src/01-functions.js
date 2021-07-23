645d405ae993983512f1cc753204e61623ff1025
Christina Loiacono <loiaconochristina@gmail.com>
2021-07-23 17:18:37 -0400

    answered first 6 functions

--- src/01-functions.js
+++ src/01-functions.js
@@ -9,7 +9,9 @@
  *  alwaysTrue();
  *  //> true
  */
-function alwaysTrue() {}
+function alwaysTrue() {
+  return true;
+}
 
 /**
  * greet()
@@ -24,7 +26,9 @@ function alwaysTrue() {}
  *  greet("Ian");
  *  //> "Hello, Ian."
  */
-function greet() {}
+function greet(name) {
+  return "Hello, " + name + ".";
+}
 
 /**
  * add()
@@ -39,7 +43,10 @@ function greet() {}
  *  add(10, 20);
  *  //> 30
  */
-function add() {}
+function add(a, b) {
+  sum = a + b;
+  return sum;
+}
 
 /**
  * multiply()
@@ -54,7 +61,10 @@ function add() {}
  *  multiply(2, 5);
  *  //> 10
  */
-function multiply() {}
+function multiply(a, b) {
+  product = a * b;
+  return product;
+}
 
 /**
  * average()
@@ -74,7 +84,10 @@ function multiply() {}
  *  average(10, 6);
  *  //> 8 // 10 + 6 = 16; 16/2 = 8;
  */
-function average() {}
+function average(a, b) {
+  average = (a + b) / 2;
+  return average;
+}
 
 /**
  * averageThree()
@@ -93,7 +106,10 @@ function average() {}
  *  averageThree(10, 11, 19);
  *  //> 10 // 10 + 11 + 19 = 30; 30 / 3 = 10;
  */
-function averageThree() {}
+function averageThree(a, b, c) {
+  average = (a + b + c) / 3;
+  return average;
+}
 
 /**
  * compareTypes()
