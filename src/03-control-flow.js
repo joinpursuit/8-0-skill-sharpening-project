be7b9076698ef9ee90e5ef506f842a46a62341e2
Samantha Gosda <gosdasam@gmail.com>
2021-07-23 15:05:52 -0400

    completed isEqual func

--- src/03-control-flow.js
+++ src/03-control-flow.js
@@ -14,7 +14,13 @@
  *  isEqual(10, "10");
  *  //> false
  */
-function isEqual(a, b) {}
+function isEqual(a, b) {
+  if(a === b){
+    return true;
+  }else{
+    return false;
+  }
+}
 
 /**
  * findLarger()
