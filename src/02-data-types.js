6913c876f4f5bc1e1bb4b7e3a033f552195b6b3c
Samantha Gosda <gosdasam@gmail.com>
2021-07-23 14:48:16 -0400

    completed convertToNumber func

--- src/02-data-types.js
+++ src/02-data-types.js
@@ -25,7 +25,9 @@ function getLengthOfString(str) {
  *  convertToNumber("111");
  *  //> 111
  */
-function convertToNumber(val) {}
+function convertToNumber(val) {
+  return Number(val);
+}
 
 /**
  * convertToString()
