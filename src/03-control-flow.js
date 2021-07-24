c7b87af3214d975422a804ce449c6e411bf3ec87
Christina Loiacono <loiaconochristina@gmail.com>
2021-07-24 15:07:54 -0400

    answered files 1, 2, and first 4 of file 3

--- src/03-control-flow.js
+++ src/03-control-flow.js
@@ -14,7 +14,13 @@
  *  isEqual(10, "10");
  *  //> false
  */
-function isEqual(a, b) {}
+function isEqual(a, b) {
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /**
  * findLarger()
@@ -28,7 +34,13 @@ function isEqual(a, b) {}
  *  findLarger(19, 7);
  *  //> 19
  */
-function findLarger(a, b) {}
+function findLarger(a, b) {
+  if (a > b) {
+    return a;
+  } else if (a < b) {
+    return b;
+  }
+}
 
 /**
  * findLargerOrTie()
@@ -45,7 +57,15 @@ function findLarger(a, b) {}
  *  findLargerOrTie(0, 0);
  *  //> "tie"
  */
-function findLargerOrTie(a, b) {}
+function findLargerOrTie(a, b) {
+  if (a > b) {
+    return a;
+  } else if (a < b) {
+    return b;
+  } else if (a === b) {
+    return "tie";
+  }
+}
 
 /**
  * positiveNegativeOrZero()
@@ -65,7 +85,15 @@ function findLargerOrTie(a, b) {}
  *  positiveNegativeOrZero(1);
  *  //> "Positive"
  */
-function positiveNegativeOrZero(a) {}
+function positiveNegativeOrZero(a) {
+  if (a > 0) {
+    return "Positive"
+  } else if (a < 0) {
+    return "Negative"
+  } else if (a === 0) {
+    return "Zero";
+  }
+}
 
 /**
  * aroundTheWorldGreeting()
@@ -128,7 +156,11 @@ function aroundTheWorldGreetingWithSwitch(language) {}
  *  calculateLetterGrade(99);
  *  //> "A"
  */
-function calculateLetterGrade(grade) {}
+function calculateLetterGrade(grade) {
+  // switch (true) {
+  //   case
+  // }
+}
 
 /**
  * animalCounts()
