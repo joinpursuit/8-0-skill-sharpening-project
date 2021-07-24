722cb72149867684cffc28df4912ef22f6cda7f9
Jalamang Janka <jalamangjankagmail.com>
2021-07-24 17:02:32 -0400

    control flow

--- src/03-control-flow.js
+++ src/03-control-flow.js
@@ -14,12 +14,19 @@
  *  isEqual(10, "10");
  *  //> false
  */
-function isEqual(a, b) {}
+function isEqual(a, b) {
+if(a === b){
+  return true;
+}
+return false;
+}
 
+ console.log(isEqual(-1, "1"))
 /**
  * findLarger()
  * ---------------------
- * Takes in two numbers and returns the larger number. The numbers are guaranteed to be unique (not equal to each other).
+ * Takes in two numbers and returns the larger number. 
+ * The numbers are guaranteed to be unique (not equal to each other).
  * @param {number} a
  * @param {number} b
  * @returns {number}
@@ -28,12 +35,21 @@ function isEqual(a, b) {}
  *  findLarger(19, 7);
  *  //> 19
  */
-function findLarger(a, b) {}
+function findLarger(a, b) {
+let largeNum = a;
+if(largeNum < b){
+  largeNum = b;
+}
+return largeNum
+}
 
+
+console.log(findLarger(-2, -1))
 /**
  * findLargerOrTie()
  * ---------------------
- * Takes in two numbers and returns the larger number. Returns the string "tie" if both numbers are the same.
+ * Takes in two numbers and returns the larger number. 
+ * Returns the string "tie" if both numbers are the same.
  * @param {number} a
  * @param {number} b
  * @returns {number|string}
@@ -45,12 +61,23 @@ function findLarger(a, b) {}
  *  findLargerOrTie(0, 0);
  *  //> "tie"
  */
-function findLargerOrTie(a, b) {}
+function findLargerOrTie(a, b) {
+  let largeOrTie = a;
+  if(largeOrTie < b){
+    largeOrTie = b;
 
+  } else if(largeOrTie === b){
+    largeOrTie = `"tie"`
+  }
+  return largeOrTie
+  }
+ 
+console.log(findLargerOrTie(2, -1))
 /**
  * positiveNegativeOrZero()
  * ---------------------
- * Takes in a number and returns whether that number is "Positive", "Negative", or "Zero".
+ * Takes in a number and returns whether that number is "Positive", 
+ * "Negative", or "Zero".
  * HINT: Capitalization and spelling count!
  * @param {number} a
  * @returns {string}
@@ -65,8 +92,20 @@ function findLargerOrTie(a, b) {}
  *  positiveNegativeOrZero(1);
  *  //> "Positive"
  */
-function positiveNegativeOrZero(a) {}
+ function positiveNegativeOrZero(a) {
+   if(typeof a !== 'number' ){
+     return `'\"${a}", not a number'`
+   }
+let thing = 'Zero';
+if(a > 0){
 
+return 'Positive'
+} else if(a < 0){
+  return 'Negative'
+}
+return thing;
+}
+ console.log(positiveNegativeOrZero(0))
 /**
  * aroundTheWorldGreeting()
  * ---------------------
@@ -86,7 +125,28 @@ function positiveNegativeOrZero(a) {}
  *  aroundTheWorldGreeting();
  *  //> "Hello World"
  */
-function aroundTheWorldGreeting(language) {}
+function aroundTheWorldGreeting(language) {
+if(language === undefined){
+  return "Hello World";
+}
+  let languageTolowerCase = language.toLowerCase();  
+  let languageGreeting = '';
+  
+
+if(languageTolowerCase === 'english'){
+  languageGreeting += "Hello World"; 
+}
+else if(languageTolowerCase === 'spanish'){
+  languageGreeting += "Hola Mundo"; 
+}
+else if(languageTolowerCase === 'german'){
+  languageGreeting += "Hallo Welt"; 
+}
+return languageGreeting
+
+}
+
+//console.log(aroundTheWorldGreeting('Spanish'))
 
 /**
  * aroundTheWorldGreetingWithSwitch()
@@ -109,12 +169,32 @@ function aroundTheWorldGreeting(language) {}
  *  aroundTheWorldGreetingWithSwitch();
  *  //> "Hello World"
  */
-function aroundTheWorldGreetingWithSwitch(language) {}
+function aroundTheWorldGreetingWithSwitch(language) {
+    if(language === undefined){
+      return "Hello World";
+    }
+      let languageTolowerCase = language.toLowerCase();  
+      let languageGreeting = "";
+      
+    switch(languageTolowerCase){
+        case 'english': languageGreeting += 'Hello World';
+        return languageGreeting;
+        break;
+        case 'spanish': languageGreeting += 'Hola Mundo';
+        return languageGreeting;
+        break;
+        case 'german': languageGreeting += 'Hallo Welt';
+        return languageGreeting; 
+        
+    }
 
+  }
+  console.log(aroundTheWorldGreetingWithSwitch())
 /**
  * calculateLetterGrade()
  * ---------------------
- * Takes in a number and returns the letter grade corresponding to that number. Letter grades consist of "A", "B", "C", "D", or "F".
+ * Takes in a number and returns the letter grade corresponding to that number. 
+ * Letter grades consist of "A", "B", "C", "D", or "F".
  *
  * SCORING: 90 and above is an A
  *          80 and above is a B
@@ -128,13 +208,41 @@ function aroundTheWorldGreetingWithSwitch(language) {}
  *  calculateLetterGrade(99);
  *  //> "A"
  */
-function calculateLetterGrade(grade) {}
+function calculateLetterGrade(grade) {
+  if(grade < 0 ){
+    return `'${grade}' cannot be a negative number`
+  }
+let letterGradeScore = '';
+if(grade < 60){
+  letterGradeScore += 'F'; 
 
+}
+else if(grade >= 90){
+  letterGradeScore += 'A'; 
+
+}
+else if(grade >= 80){
+  letterGradeScore += 'B'; 
+
+}
+else if(grade >= 70){
+  letterGradeScore += 'C'; 
+
+}
+else if(grade >= 60){
+  letterGradeScore += 'D'; 
+
+}
+return letterGradeScore;
+}
+console.log(calculateLetterGrade(5))
 /**
  * animalCounts()
  * ---------------------
- * Returns the count of animals, properly pluralizing the animal if there is more than one.
- * The animal is guaranteed to be one you can pluralize by adding `s`. (NO geese, fish, deer, oxen, etc.)
+ * Returns the count of animals, properly pluralizing the animal 
+ * if there is more than one.
+ * The animal is guaranteed to be one you can pluralize by adding `s`.
+ * (NO geese, fish, deer, oxen, etc.)
  * @param {string} animal
  * @param {number} numberOfAnimals
  * @returns {string}
@@ -146,8 +254,23 @@ function calculateLetterGrade(grade) {}
  *  animalCounts("pig", 1);
  *  //> "1 pig"
  */
-function animalCounts(animal, numberOfAnimals) {}
+function animalCounts(animal, numberOfAnimals) {
+  if(numberOfAnimals === 0){
+    return `There's no animal`
+  }
 
+let numberOfAnimal = '';
+if(numberOfAnimals === 1){
+  numberOfAnimal += `"${numberOfAnimals} ${animal}"`
+} 
+else if(numberOfAnimals > 1){
+  numberOfAnimal += `"${numberOfAnimals} ${animal}s"`
+}
+
+return numberOfAnimal;
+}
+
+console.log(animalCounts("dolphin", 6))
 /**
  * evenOrOdd()
  * ---------------------
@@ -163,8 +286,19 @@ function animalCounts(animal, numberOfAnimals) {}
  *  evenOrOdd(48);
  *  //> "Even"
  */
-function evenOrOdd(a) {}
+function evenOrOdd(a) {
+let typeOfNumber = '';
+if(a % 2 === 1 || a % 2 === -1){
+  typeOfNumber += `"Odd"`
+} 
+else if(a % 2 === 0){
+  typeOfNumber +=`"Even"`
 
+}
+return typeOfNumber;
+}
+
+ console.log(evenOrOdd(-2))
 /**
  * evenOrOddWithTernary()
  * ---------------------
@@ -179,8 +313,18 @@ function evenOrOdd(a) {}
  *  evenOrOddWithTernary(8);
  *  //> "Even"
  */
-function evenOrOddWithTernary(a) {}
+function evenOrOddWithTernary(a) {
 
+  if(a === -1){
+    return '"Odd"';
+  }
+let typeOfNumber = ''
+typeOfNumber += a % 2 === 1? '"Odd"' : '"Even"';
+
+return typeOfNumber;
+}
+
+console.log(evenOrOddWithTernary(9))
 // Do not change any code below this line.
 module.exports = {
   isEqual,
