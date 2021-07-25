f5fdc807c9f1a408b14caa66ad31d8df6f7deda8
DylanLovell <arcanyne7@gmail.com>
2021-07-25 19:28:03 -0400

    accessing

--- src/08-accessing.js
+++ src/08-accessing.js
@@ -32,7 +32,11 @@
  *  getFirstName(person);
  *  //> "Rachel"
  */
-function getFirstName(person) {}
+function getFirstName(person) {
+  //use dotnotation to access the names value, and the first name that's inside that object
+  //return it
+  return person.names.first;
+}
 
 /**
  * getLastName()
@@ -45,7 +49,11 @@ function getFirstName(person) {}
  *  getLastName(person);
  *  //> "Rojas"
  */
-function getLastName(person) {}
+function getLastName(person) {
+  //use dotnotation to access the names value, and the last name inside of that object
+  //return it
+  return person.names.last;
+}
 
 /**
  * getAddressStreet()
@@ -58,7 +66,11 @@ function getLastName(person) {}
  *  getAddressStreet(person);
  *  //> "697 Pine Drive"
  */
-function getAddressStreet(person) {}
+function getAddressStreet(person) {
+  //use dotnotion to access the address value, and again to access the street
+  //return it
+  return person.address.street;
+}
 
 /**
  * getCountOfPhoneNumbers()
@@ -72,6 +84,7 @@ function getAddressStreet(person) {}
  *  //> 2
  */
 function getCountOfPhoneNumbers(person) {
+  //use dotnotation to access the numbers value, and .length to get the length of the array
   return person.numbers.length;
 }
 
@@ -89,7 +102,11 @@ function getCountOfPhoneNumbers(person) {
  *  getFirstPhoneNumber(person);
  *  //> 7185550921
  */
-function getFirstPhoneNumber(person) {}
+function getFirstPhoneNumber(person) {
+  //use dot notation to access the numbers value, and then bracket notation to access the first value in the array
+  //return it
+  return person.numbers[0];
+}
 
 /**
  * getLastPhoneNumber()
@@ -105,7 +122,11 @@ function getFirstPhoneNumber(person) {}
  *  getLastPhoneNumber(person);
  *  //> 7185558611
  */
-function getLastPhoneNumber(person) {}
+function getLastPhoneNumber(person) {
+  //use dot notation to access the numbers value, and then bracket notation to access the first value in the array
+  //return it
+  return person.numbers[person.numbers.length - 1];
+}
 
 /**
  * getFullName()
@@ -118,7 +139,12 @@ function getLastPhoneNumber(person) {}
  *  getFullName(person);
  *  //> "Rachel Eleanor Rojas"
  */
-function getFullName(person) {}
+function getFullName(person) {
+  //Access the names via dot notation and interpolate them into a string
+  //return it
+  return `${person.names.first} ${person.names.middle} ${person.names.last}`;
+
+}
 
 /**
  * getCityAndState()
@@ -134,7 +160,11 @@ function getFullName(person) {}
  *  getCityAndState(person);
  *  //> "Staten Island, NY"
  */
-function getCityAndState(person) {}
+function getCityAndState(person) {
+  //access the city and state via dot notation on the person and address objects
+  // interpolate them into a string and return it
+  return `${person.address.city}, ${person.address.state}`
+}
 
 /**
  * getFullAddress()
@@ -150,7 +180,11 @@ function getCityAndState(person) {}
  *  getFullAddress(person);
  *  //> "697 Pine Drive 2A, Staten Island, NY, 10306"
  */
-function getFullAddress(person) {}
+function getFullAddress(person) {
+  //Access the address information via dot notation in the person object
+  //interpolate it, and return it
+  return `${person.address.street} ${person.address.unit}, ${person.address.city}, ${person.address.state}, ${person.address.zip}`
+}
 
 /**
  * getFlatObject()
@@ -173,8 +207,29 @@ function getFullAddress(person) {}
       numbers: [7185550921, 7185558611],
     };
  */
-function getFlatObject(person) {}
-
+function getFlatObject(person) {
+  //declare result variable to accumulate to, empty object
+  let result = {};
+  //declare loop to iterate through object
+  for (const info in person){
+    //in 1st loop, check if the value is an object, and NOT an array
+    if (typeof person[info] === 'object' && !Array.isArray(person[info])){
+      //create variable for clarity
+      let toFlatten = person[info];
+      //declare 2nd loop for our object to flatten
+      for (const key in toFlatten){
+        //in second loop, send every key value pair to the result object
+        result[key] = toFlatten[key];
+      }
+    //after 2nd loop, if the value WASNT an object
+    } else { 
+      //send the key and value to our result array
+      result[info] = person[info];
+    }
+  }
+  //after looping, return our result
+  return result; 
+}
 // Do not change the code below.
 module.exports = {
   getFirstName,
