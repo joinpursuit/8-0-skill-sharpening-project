8f923950be7e86c537544d6e2c585544dab7eddc
Sara Perkaj <saraperkaj@pursuit.org>
2021-07-25 15:37:11 -0400

    First two done, just running tests for practice.

--- src/01-functions.js
+++ src/01-functions.js
@@ -9,7 +9,10 @@
  *  alwaysTrue();
  *  //> true
  */
-function alwaysTrue() {}
+function alwaysTrue() {
+  //this will ALWAYS return true
+  return true;
+}
 
 /**
  * greet()
@@ -24,7 +27,12 @@ function alwaysTrue() {}
  *  greet("Ian");
  *  //> "Hello, Ian."
  */
-function greet() {}
+function greet(name) {
+// decalre a varand assign it "Hello,"
+let greeting = "Hello, "
+//return the var + name
+  return greeting + name;
+}
 
 /**
  * add()
@@ -39,7 +47,9 @@ function greet() {}
  *  add(10, 20);
  *  //> 30
  */
-function add() {}
+function add(a, b) {
+//
+}
 
 /**
  * multiply()
@@ -54,7 +64,9 @@ function add() {}
  *  multiply(2, 5);
  *  //> 10
  */
-function multiply() {}
+function multiply() {
+
+}
 
 /**
  * average()
@@ -74,7 +86,9 @@ function multiply() {}
  *  average(10, 6);
  *  //> 8 // 10 + 6 = 16; 16/2 = 8;
  */
-function average() {}
+function average() {
+
+}
 
 /**
  * averageThree()
@@ -93,7 +107,9 @@ function average() {}
  *  averageThree(10, 11, 19);
  *  //> 10 // 10 + 11 + 19 = 30; 30 / 3 = 10;
  */
-function averageThree() {}
+function averageThree() {
+
+}
 
 /**
  * compareTypes()
@@ -112,7 +128,9 @@ function averageThree() {}
  *  compareTypes("left", 10);
  *  //> false
  */
-function compareTypes() {}
+function compareTypes() {
+
+}
 
 /**
  * flipSign()
@@ -131,7 +149,9 @@ function compareTypes() {}
  *  flipSign(50);
  *  //> -50
  */
-function flipSign() {}
+function flipSign() {
+
+}
 
 /**
  * joinStringsWithSpaces()
@@ -151,7 +171,9 @@ function flipSign() {}
  *  joinStringsWithSpaces("left", "right", "up", "down", "away");
  *  //> "left right up down away"
  */
-function joinStringsWithSpaces() {}
+function joinStringsWithSpaces() {
+
+}
 
 /**
  * getFirstAndLastCharacter()
@@ -168,7 +190,9 @@ function joinStringsWithSpaces() {}
  *  getFirstAndLastCharacter("upwards");
  *  //> "us"
  */
-function getFirstAndLastCharacter() {}
+function getFirstAndLastCharacter() {
+
+}
 
 // Do not change the code below.
 module.exports = {
