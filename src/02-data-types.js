02327c704213ea7f80eb72259b1152ce05ae8b12
Milton Clark <kingcoder247@gmail.com>
2021-07-26 19:05:31 -0400

    Function isEven passed

--- src/02-data-types.js
+++ src/02-data-types.js
@@ -110,7 +110,14 @@ function checkIfCharacterIsInString(text, character) {
  *  isEven(11);
  *  //> false
  */
-function isEven(num) {}
+
+function isEven(num) {
+  if(num % 2 == 0){
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /**
  * isOdd()
