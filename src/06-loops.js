43970ddcf60a98b4587111970541166577ac51e4
stephanie lucano <stephaniemarielucano@gmail.com>
2021-07-26 10:09:01 -0400

    Passed all the loops tests

--- src/06-loops.js
+++ src/06-loops.js
@@ -11,7 +11,22 @@
  *  //> 25
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
-function rangeSum(min, max) {}
+function rangeSum(min, max) {
+  // output: return the sum of range between `min` and `max`
+
+  // problem: How do I get the range between `min` and `max`
+  // solution: 1. loop: let i = min, set condition so i stops at max , increment by one
+
+  // declare a variable `sumOfRange` and assign it min
+  let sumOfRange = 0
+  // loop through numbers between `min++` and `max` (inclusive)
+  for (let i = min; i <= max; i++) {
+    // reassign sumOfRange using += to 
+    sumOfRange += i
+  }
+  // return sumOfRange
+  return sumOfRange
+}
 
 /**
  * rangeOdd()
@@ -25,7 +40,26 @@ function rangeSum(min, max) {}
  *  rangeOdd(10, 15);
  *  //> [ 15, 13, 11 ]
  */
-function rangeOdd(min, max) {}
+function rangeOdd(min, max) {
+  // output: return an aray of numbers in descending order of only the odd numbers between `max` and `min`
+
+  // problem: How are we going to filter out the odd numbers between `max` and `min`
+  // solution: 1. loop 2. If the number is odd push it to arr 
+
+  // declare a variable `cacheArr` and assign it an empty arr
+  let cacheArr = []
+  // loop through numbers starting from `max` all the way down to `min` 
+  for (let i = max; i >= min; i--) {
+    // if the number is odd
+    if (i % 2 === 1) {
+      // `push` the odd numbers into `cacheArr`
+      cacheArr.push(i)
+    }
+  }
+  
+  // return cacheArr
+  return cacheArr
+}
 
 /**
  * rangeEveryOther()
@@ -39,7 +73,22 @@ function rangeOdd(min, max) {}
  *  rangeEveryOther(11, 18);
  *  //> [ 11, 13, 15, 17 ]
  */
-function rangeEveryOther(min, max) {}
+function rangeEveryOther(min, max) {
+  // output: return an array of numbers in increasing order skipping every other number starting from `min` 
+
+  // problem: How would you create a loop to skip every other number?
+  // solution: 
+
+  // declare a variable `cacheArr` and assign it an empty arr
+  let cacheArr = []
+  // loop through numbers starting from `min` and ending at `max`
+  for (let i = min; i <= max; i += 2) {
+    // push the number into `cacheArr`
+    cacheArr.push(i)
+  }
+  // return cacheArr
+  return cacheArr
+}
 
 /**
  * containsWhileLoop()
@@ -61,7 +110,24 @@ function rangeEveryOther(min, max) {}
  *  containsWhileLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsWhileLoop(array, target) {}
+function containsWhileLoop(array, target) {
+  // output: return true if the `target` valie exists in the `array`
+
+  // declare a variable `arrayIndex` assign it 0
+  let i = 0
+  // loop through the array
+  while (i < array.length) {
+    // if the target value is equal to one of the element in the array
+    if (target === array[i]) {
+      // return true
+      return true
+    }
+    i++
+  }
+  
+  // otherwise return false
+  return false
+}
 
 /**
  * containsForLoop()
@@ -83,7 +149,20 @@ function containsWhileLoop(array, target) {}
  *  containsForLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsForLoop(array, target) {}
+function containsForLoop(array, target) {
+  // ouput: return true if the target value exists in the `array`
+
+  // for loop and iterate through `array`
+  for (let i = 0; i < array.length; i++) {
+    // if target value is strictly equal to one of the element in the array
+    if (target === array[i]) {
+      // return true
+      return true
+    }
+  }
+  // otherwise return false
+  return false
+}
 
 /**
  * targetCount()
@@ -97,7 +176,22 @@ function containsForLoop(array, target) {}
  *  targetCount([ 10, 20, 10, 20, 30 ], 10);
  *  //> 2
  */
-function targetCount(array, target) {}
+function targetCount(array, target) {
+  // output: return the number of times the `target` value occurs in the `array`
+
+  // declare a variable count and assign it 0
+  let count = 0
+  // loop through array
+  for (let i = 0; i < array.length; i++) {
+    // if the target value is equal to on of the element in the array
+    if (target === array[i]) {
+      // reassign the count up by one
+      count++
+    }
+  }
+  // return count
+  return count
+}
 
 /**
  * firstIndexFound()
@@ -117,7 +211,20 @@ function targetCount(array, target) {}
  *  firstIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function firstIndexFound(array, target) {}
+function firstIndexFound(array, target) {
+  // output: returns index number if `target` value is found in the array otherwise return -1
+
+  // loop through array
+  for (let i = 0; i < array.length; i++) {
+    // if the target value is strinctly equal to the dirst index in the array
+    if (target === array[i]) {
+      // return index at which target value is found
+      return i
+    }
+  }
+  // otherwise return -1
+  return -1
+}
 
 /**
  * lastIndexFound()
@@ -137,7 +244,20 @@ function firstIndexFound(array, target) {}
  *  lastIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function lastIndexFound(array, target) {}
+function lastIndexFound(array, target) {
+  // output: return the index number if the target value is found otherwise return -1
+
+  // loop through array
+  for (let i = array.length - 1; i >= 0; i-- ) {
+    // if the target value is found   
+    if (target === array[i]) {
+      // return the index of the array
+      return i
+    }
+  }
+  // otherwise return -1
+  return -1
+}
 
 /**
  * timesIndex()
@@ -151,7 +271,23 @@ function lastIndexFound(array, target) {}
  *  //> [ 7 * 0, 10 * 1, 11 * 2 ]
  *  //> [ 0, 10, 22 ]
  */
-function timesIndex(array) {}
+function timesIndex(array) {
+  // output: return the product of element and it's index in a new array of numbers 
+
+  // problem: How do you multiply the element and the index and push it in a new array
+  // solution: 1. loop 2. artmetic operator 3. empty array to push the product into as an element
+
+  // declare a `cacheArr` and assign it an empty arr
+  let cacheArr = []
+  // loop through `array`
+  for (let i = 0; i < array.length; i++) {
+    // multiply the element by it's index 
+    // push the product into cache arr
+    cacheArr.push(array[i] * i)
+  }
+  // return cacheArr
+  return cacheArr
+}
 
 /**
  * cumulativeSum()
@@ -165,7 +301,23 @@ function timesIndex(array) {}
  *  //> [ 5, 5 + 2, 5 + 2 + 9 ]
  *  //> [ 5, 7, 16 ]
  */
-function cumulativeSum(array) {}
+function cumulativeSum(array) {
+  // output: return a new array with the sum of the element and index up until THAT POINT
+
+  // edge case for empty array
+  if (array.length === 0) {
+    // return empty array
+    return []
+  }
+  // declare a variable `cacheArr` and assign it the first element in the `array`
+  let cacheArr = [array[0]]
+  // loop through `array`
+  for (let i = 1; i < array.length; i++) {
+      cacheArr.push(cacheArr[cacheArr.length -1] + array[i])
+  }
+    //return cacheArr
+    return cacheArr
+}
 
 // Do not change anything below this line.
 module.exports = {
