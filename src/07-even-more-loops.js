9d708a34e7f8f95cd46bcb2168a7bb649f678c6d
Christina Loiacono <loiaconochristina@gmail.com>
2021-07-27 12:43:37 -0400

    added code to even more loops

--- src/07-even-more-loops.js
+++ src/07-even-more-loops.js
@@ -12,7 +12,16 @@
  *  shoutForLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForLoop(array) {}
+function shoutForLoop(array) {
+  let addedNums = []; // make a new array
+  let sum = "";
+  
+  for (let i = 0; i < array.length; i++) { // loops
+    sum = array[i] + "!";
+    addedNums.push(sum);
+  }
+ return addedNums; // return new array - outside for loop
+}
 
 /**
  * shoutWhileLoop()
@@ -28,7 +37,17 @@ function shoutForLoop(array) {}
  *  shoutWhileLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutWhileLoop(array) {}
+function shoutWhileLoop(array) {
+  let addedNums = [];
+  let i = 0;
+  
+  while (i < array.length) {
+    sum = array[i] + "!";
+    i++;
+    addedNums.push(sum);
+  }
+ return addedNums;
+}
 
 /**
  * shoutForOfLoop()
@@ -44,7 +63,16 @@ function shoutWhileLoop(array) {}
  *  shoutForOfLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForOfLoop(array) {}
+function shoutForOfLoop(array) {
+  let addedNums = [];
+  
+  for (let element of array) {
+    element += "!"; // element = array[i]
+    addedNums.push(element);
+  }
+ return addedNums;
+  
+}
 
 /**
  * sumArray()
@@ -57,7 +85,13 @@ function shoutForOfLoop(array) {}
  *  sumArray([ 10, 0, 10, 11 ]);
  *  //> 31
  */
-function sumArray(array) {}
+function sumArray(array) {
+  let addedNums = 0; // number
+  for (let element of array) {
+    addedNums += element;
+  }
+  return addedNums; // number
+}
 
 /**
  * oddArray()
@@ -70,7 +104,15 @@ function sumArray(array) {}
  *  oddArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 11, 15, 37 ]
  */
-function oddArray(array) {}
+function oddArray(array) {
+  let addedNums = []; // array
+  for (let element of array) {
+    if (element % 2 === 1) { // odd
+      addedNums.push(element);
+    }
+  }
+  return addedNums; // array
+}
 
 /**
  * evenArray()
@@ -83,7 +125,15 @@ function oddArray(array) {}
  *  evenArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 20, 22 ]
  */
-function evenArray(array) {}
+function evenArray(array) {
+  let addedNums = []; // array
+  for (let element of array) {
+    if (element % 2 === 0) { // odd
+      addedNums.push(element);
+    }
+  }
+  return addedNums; // array
+}
 
 /**
  * findSmallest()
@@ -96,7 +146,15 @@ function evenArray(array) {}
  *  findSmallest([ 0, 11, -2, 5 ]);
  *  //> -2
  */
-function findSmallest(array) {}
+function findSmallest(array) {
+  let smallestNum = array[0]; // number
+  for (let element of array) {
+    if (element < smallestNum) { 
+      smallestNum = element;
+    }
+  }
+  return smallestNum; // number
+}
 
 /**
  * findLargest()
@@ -109,7 +167,15 @@ function findSmallest(array) {}
  *  findLargest([ 0, 11, -2, 5 ]);
  *  //> 11
  */
-function findLargest(array) {}
+function findLargest(array) {
+  let largestNum = array[0]; // number
+  for (let element of array) {
+    if (element > largestNum) { 
+      largestNum = element;
+    }
+  }
+  return largestNum; // number
+}
 
 /**
  * findEqual()
@@ -126,8 +192,18 @@ function findLargest(array) {}
  *  findEqual([ 0, 11, -2, 5 ], 9);
  *  //> false
  */
-function findEqual(array, selected) {}
-
+function findEqual(array, selected) {
+  let equalNum = 0; // number
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === selected) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return equalNum; // number - outside for loop
+}
+findEqual([ 0, 11, -2, 5 ], 11);
 /**
  * removeDuplicates()
  * ---------------------
@@ -143,7 +219,15 @@ function findEqual(array, selected) {}
  *  //> [ 1, 11, 2, 3, 4, 9 ]
  */
 
-function removeDuplicates(array) {}
+function removeDuplicates(array) {
+  let equalNum = 0; // number
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[i]) {
+      element.splice(i, 1);
+    }
+  }
+  return equalNum; // number - outside for loop
+}
 
 // Do not change any code below this line.
 module.exports = {
