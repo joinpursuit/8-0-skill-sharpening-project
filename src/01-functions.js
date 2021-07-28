44f99a83f46f0aefe7cc5c0eb61576968e828a87
stephanie lucano <stephaniemarielucano@gmail.com>
2021-07-28 14:26:50 -0400

    Refactor and add semicolons to functions

--- src/01-functions.js
+++ src/01-functions.js
@@ -29,14 +29,17 @@ function alwaysTrue() {
 function greet(name) {
   // input: name is a string
   // output: return a string 'Hello, Name'
-  
-  // problem: How do you capitalize the letter for first name only?
-  // solution: 1. create a string copy in array 2. use string concatenation - to add the rest of the name
-  // variable: declare a variable to make a string copy in an array
+
+  // edge case: How do you capitalize the first letter of name and have the rest of the name lowercased?
+  // solution: 1. create a string copy in an array 2. string concatenation 
+  // variable: strCopy of name in an array, firstName to hold the concatenated first letter with rest of name
+
+  // declare a variable and assign it a string copy in an array
   const strCopy = name.split();
-  // The first letter needs to be accessed and capitalized in strCopy then add all the other letters to the first letter
-  const firstName = strCopy[0][0].toUpperCase() + strCopy[0].slice(1)
-  // return firstName with greeting  "Hello, "
+  // declare a varaible and assign it the first letter capitalized and concatenate all the other lowercased letters
+  const firstName = strCopy[0][0].toUpperCase() + strCopy[0].slice(1).toLowerCase()
+
+  // return firstName with greeting 'Hello, Name'
   return `Hello, ${firstName}.`;
 }
 
@@ -57,7 +60,7 @@ function add(a, b) {
   // input: a and b are numbers
   // output: return a number that adds a and b
 
-  return a + b
+  return a + b;
 }
 
 /**
@@ -77,7 +80,7 @@ function multiply(a ,b) {
   // input: a and b are numbers
   // output: return the product of a and b
 
-  return a * b
+  return a * b;
 }
 
 /**
@@ -102,7 +105,7 @@ function average(a ,b) {
   // input: a and b are numbers
   // output: return the average of the two numbers
 
-  return (a + b) / 2
+  return (a + b) / 2;
 }
 
 /**
@@ -126,7 +129,7 @@ function averageThree(a , b, c) {
   // input: a, b and c are numbers
   // output: return a number that returns the average of the three numbers
 
-  return (a + b + c) / 3
+  return (a + b + c) / 3;
 }
 
 /**
@@ -159,7 +162,7 @@ function compareTypes(a , b) {
   // return false
 
   // === stricly equal to operator returns a boolean 
-  return typeof a === typeof b
+  return typeof a === typeof b;
 }
 
 /**
@@ -184,21 +187,24 @@ function flipSign(a) {
   // output: return the negative `number` if the `number` is a positive integer or 
   //         return the positive `number` if the `number` is a negative integer
 
-  // if the number is a positive integer
-  if (a > 0) {
-    // return a negative `number`
-    return - a
+  // // if the number is a positive integer
+  // if (a > 0) {
+  //   // return a negative `number`
+  //   return - a
 
-    // if the `number` is a negative integer
-  } else if (a < 0) {
-    // return a positive `number`
-    return -a
+  //   // if the `number` is a negative integer
+  // } else if (a < 0) {
+  //   // return a positive `number`
+  //   return - a
 
-    // edge case if 0
-  } else {
-    // return `0 is a neutral number`
-    return '0 is a neutral number.'
-  }
+  //   // edge case if 0
+  // } else {
+  //   // return `0 is a neutral number`
+  //   return '0 is a neutral number.'
+  // }
+
+  // The same return for first two conditional statements
+  return - a;
 }
 
 /**
@@ -221,17 +227,20 @@ function flipSign(a) {
  */
 function joinStringsWithSpaces(a, b, c, d, e) {
   // input: a, b, c, d and e are strings
-  // output: return a concatenated string
+  // output: return a concatenated string or template literal
 
   // problem: what is the simplest way to combine two strings?
-  // solution: string concatenation
-  // declare a variab;e called space and assign it a empty string with one space in between
-  const space = ' '
-  // declare the variable called string thread 
-  const stringThread = a + space + b + space + c + space + d + space + e
+  // solution: 1. x string concatenation 2. template literals 
 
-  // return the concatenated string
-  return stringThread
+  // // declare a variab;e called space and assign it a empty string with one space in between
+  // const space = ' '
+  // // declare the variable called string thread 
+  // const stringThread = a + space + b + space + c + space + d + space + e
+
+  // // return the concatenated string
+  // return stringThread
+
+  return `${a} ${b} ${c} ${d} ${e}`;
 }
 
 /**
@@ -254,12 +263,13 @@ function getFirstAndLastCharacter(a) {
   // output: returns a string with the first and last character of string a
 
   // problem: How can you find the nth character of any string?
-  // solution: 1. String concatenation 2. string.prototype.charAt()
-  // declare a variable `firstAndLastLetter` of the string using string concatenation
-  const firstAndLastLetter =  a.charAt(0) + a.charAt(a.length - 1)
-  // return first and last letter
-  return firstAndLastLetter
+  // solution: 1. String concatenation 2. x string.prototype.charAt() or string indexes
 
+  // declare a variable `firstAndLastLetter` of the string using string concatenation
+  const firstAndLastLetter =  a[0] + a[a.length - 1];
+
+  // return first and last letter
+  return firstAndLastLetter;
 }
 
 // Do not change the code below.
