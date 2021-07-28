e3a6755f340d5466b087e46737d545387136ea8c
Milton Clark <kingcoder247@gmail.com>
2021-07-28 18:21:46 -0400

    Function deleteKeyFromObject Passed

--- src/05-objects.js
+++ src/05-objects.js
@@ -106,13 +106,6 @@ function getAgeFromObject(object) {
  *  //> { left: true, key: false }
  */
 
-//  obj["key3"] = "value3";
-//  var getProperty = function (propertyName) {
-//   return obj[propertyName];
-// };
-
-
-
 function addValueToObject(object, val) {
   object["newKey"] = val;
   return object;
@@ -148,7 +141,10 @@ function addKeyToObject(object, key) {
  *  addKeyValuePairToObject({ up: true }, "left", false);
  *  //> { up: true, left: false }
  */
-function addKeyValuePairToObject(object, key, val) {}
+function addKeyValuePairToObject(object, key, val) {
+  object[key] = val;
+  return object;
+}
 
 /**
  * deleteKeyFromObject()
@@ -162,7 +158,10 @@ function addKeyValuePairToObject(object, key, val) {}
  *  deleteKeyFromObject({ left: true, right: false }, "left");
  *  //> { right: false }
  */
-function deleteKeyFromObject(object, key) {}
+function deleteKeyFromObject(object, key) {
+  delete object[key];
+  return object;
+}
 
 // Do not change any code below this line.
 module.exports = {
