ca5fde322de3322f9524cc77a3524483de18269e
AlexTan21 <87435891+AlexTan21@users.noreply.github.com>
2021-07-27 22:14:33 -0400

    Add files via upload

--- src/05-objects.js
+++ src/05-objects.js
@@ -9,7 +9,10 @@
  *  createEmptyObject()
  *  //> {}
  */
-function createEmptyObject() {}
+function createEmptyObject() {
+  let object = {}
+  return object
+}
 
 /**
  * createObjectWithValue()
@@ -22,7 +25,10 @@ function createEmptyObject() {}
  *  createObjectWithValue(19);
  *  //> { whateverKey: 19 }
  */
-function createObjectWithValue(val) {}
+function createObjectWithValue(val) {
+  let object = {val}
+  return object
+}
 
 /**
  * createObjectWithKey()
@@ -35,7 +41,10 @@ function createObjectWithValue(val) {}
  *  createObjectWithKey("left");
  *  //> { left: false }
  */
-function createObjectWithKey(key) {}
+function createObjectWithKey(key) {
+  let object = {[key]:""}
+  return object
+}
 
 /**
  * createObjectWithKeyValuePair()
@@ -49,7 +58,10 @@ function createObjectWithKey(key) {}
  *  createObjectWithKeyValuePair("left", "right");
  *  //> { left: "right" }
  */
-function createObjectWithKeyValuePair(key, val) {}
+function createObjectWithKeyValuePair(key, val) {
+  let object = {[key]:val}
+  return object
+}
 
 /**
  * getNameFromObject()
@@ -62,7 +74,9 @@ function createObjectWithKeyValuePair(key, val) {}
  *  getNameFromObject({ name: "Jay" });
  *  //> "Jay"
  */
-function getNameFromObject(object) {}
+function getNameFromObject(object) {
+  return object.name
+}
 
 /**
  * getAgeFromObject()
@@ -75,7 +89,9 @@ function getNameFromObject(object) {}
  *  getAgeFromObject({ age: 30 });
  *  //> 30
  */
-function getAgeFromObject(object) {}
+function getAgeFromObject(object) {
+  return object.age
+}
 
 /**
  * addValueToObject()
@@ -89,7 +105,10 @@ function getAgeFromObject(object) {}
  *  addValueToObject({ left: true }, false);
  *  //> { left: true, key: false }
  */
-function addValueToObject(object, val) {}
+function addValueToObject(object, val) {
+  object.val = val
+  return object
+}
 
 /**
  * addKeyToObject()
@@ -103,7 +122,10 @@ function addValueToObject(object, val) {}
  *  addKeyToObject({ right: false }, "left");
  *  //> { right: false, left: true }
  */
-function addKeyToObject(object, key) {}
+function addKeyToObject(object, key) {
+  object[key] = key
+  return object
+}
 
 /**
  * addKeyValuePairToObject()
@@ -118,7 +140,10 @@ function addKeyToObject(object, key) {}
  *  addKeyValuePairToObject({ up: true }, "left", false);
  *  //> { up: true, left: false }
  */
-function addKeyValuePairToObject(object, key, val) {}
+function addKeyValuePairToObject(object, key, val) {
+  object[key] = val
+  return object
+}
 
 /**
  * deleteKeyFromObject()
@@ -132,7 +157,10 @@ function addKeyValuePairToObject(object, key, val) {}
  *  deleteKeyFromObject({ left: true, right: false }, "left");
  *  //> { right: false }
  */
-function deleteKeyFromObject(object, key) {}
+function deleteKeyFromObject(object, key) {
+  delete object[key]
+  return object
+}
 
 // Do not change any code below this line.
 module.exports = {
--- src/07-even-more-loops.js
+++ src/07-even-more-loops.js
@@ -12,7 +12,16 @@
  *  shoutForLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForLoop(array) {}
+function shoutForLoop(array) {
+       let newArray = [];
+     for (let i = 0; i < array.length; i++){
+         newArray += array[i];
+         if (i !== array.length-1){
+             newArray += "! ";
+         }
+     }
+     return newArray;
+}
 
 /**
  * shoutWhileLoop()
@@ -28,7 +37,18 @@ function shoutForLoop(array) {}
  *  shoutWhileLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutWhileLoop(array) {}
+function shoutWhileLoop(array) {
+  let newArray = [];
+  let i = 0
+  while (i < array.length){
+    i++;
+      newArray += array[i];
+      if (i !== array.length-1){
+          newArray += "! ";
+      }
+  }
+  return newArray;
+}
 
 /**
  * shoutForOfLoop()
@@ -44,7 +64,15 @@ function shoutWhileLoop(array) {}
  *  shoutForOfLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForOfLoop(array) {}
+function shoutForOfLoop(array) {
+  for (newArray of array){
+      newArray += array[i];
+      if (i !== array.length-1){
+          newArray += "! ";
+      }
+  }
+  return newArray;
+}
 
 /**
  * sumArray()
@@ -57,7 +85,13 @@ function shoutForOfLoop(array) {}
  *  sumArray([ 10, 0, 10, 11 ]);
  *  //> 31
  */
-function sumArray(array) {}
+function sumArray(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum
+}
 
 /**
  * oddArray()
@@ -70,7 +104,9 @@ function sumArray(array) {}
  *  oddArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 11, 15, 37 ]
  */
-function oddArray(array) {}
+function oddArray(array) {
+  return array.filter(i => i % 2 === 1)
+}
 
 /**
  * evenArray()
@@ -83,7 +119,9 @@ function oddArray(array) {}
  *  evenArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 20, 22 ]
  */
-function evenArray(array) {}
+function evenArray(array) {
+  return array.filter(i => i % 2 === 0)
+}
 
 /**
  * findSmallest()
@@ -96,7 +134,16 @@ function evenArray(array) {}
  *  findSmallest([ 0, 11, -2, 5 ]);
  *  //> -2
  */
-function findSmallest(array) {}
+function findSmallest(array) {
+  let number = array[0];
+  for(let i = 0; i < array.length; i++){
+      let numbers = array[i];
+      if(numbers < number){
+          number = numbers;
+      }
+  }
+  return number;
+}
 
 /**
  * findLargest()
@@ -109,7 +156,16 @@ function findSmallest(array) {}
  *  findLargest([ 0, 11, -2, 5 ]);
  *  //> 11
  */
-function findLargest(array) {}
+function findLargest(array) {
+       let number = 0;
+     for(let i = 0; i < array.length; i++){
+         let numbers = array[i];
+         if(numbers > number){
+             number = numbers;
+         }
+     }
+     return number;
+}
 
 /**
  * findEqual()
@@ -126,7 +182,9 @@ function findLargest(array) {}
  *  findEqual([ 0, 11, -2, 5 ], 9);
  *  //> false
  */
-function findEqual(array, selected) {}
+function findEqual(array, selected) {
+  return array.includes(selected)
+}
 
 /**
  * removeDuplicates()
@@ -143,7 +201,10 @@ function findEqual(array, selected) {}
  *  //> [ 1, 11, 2, 3, 4, 9 ]
  */
 
-function removeDuplicates(array) {}
+function removeDuplicates(array) {
+  let newArray = [...new Set(array)]
+  return newArray
+}
 
 // Do not change any code below this line.
 module.exports = {
