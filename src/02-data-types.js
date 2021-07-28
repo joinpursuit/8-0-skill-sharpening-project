5d28c96253d37ef027f3b4b423a0a3e1391abcf7
Stephanie <stephaniereyes@pursuit.org>
2021-07-28 03:10:38 -0400

    data types

--- src/02-data-types.js
+++ src/02-data-types.js
@@ -9,7 +9,9 @@
  *  getLengthOfString("down");
  *  //> 4
  */
-function getLengthOfString(str) {}
+function getLengthOfString(str) {
+  return str.length
+}
 
 /**
  * convertToNumber()
@@ -22,7 +24,9 @@ function getLengthOfString(str) {}
  *  convertToNumber("111");
  *  //> 111
  */
-function convertToNumber(val) {}
+function convertToNumber(val) {
+  return Number(val)
+}
 
 /**
  * convertToString()
@@ -36,7 +40,9 @@ function convertToNumber(val) {}
  *  convertToString(99);
  *  //> "99"
  */
-function convertToString(val) {}
+function convertToString(val) {
+  return String(val)
+}
 
 /**
  * convertToShoutingText()
@@ -49,7 +55,9 @@ function convertToString(val) {}
  *  convertToShoutingText("Hello There");
  *  //> "HELLO THERE"
  */
-function convertToShoutingText(text) {}
+function convertToShoutingText(text) {
+return text.toUpperCase();
+}
 
 /**
  * convertToWhisperText()
@@ -62,7 +70,9 @@ function convertToShoutingText(text) {}
  *  convertToWhisperText("Hello There");
  *  //> "hello there"
  */
-function convertToWhisperText(text) {}
+function convertToWhisperText(text) {
+  return text.toLowerCase();
+}
 
 /**
  * checkIfCharacterIsInString()
@@ -79,7 +89,12 @@ function convertToWhisperText(text) {}
  *  checkIfCharacterIsInString("hello there", "a");
  *  //> false
  */
-function checkIfCharacterIsInString(text, character) {}
+function checkIfCharacterIsInString(text, character) {
+  let isInStr = false;
+  for(let i=0;i<text.length;i++){
+
+  }
+}
 
 /**
  * isEven()
@@ -95,7 +110,10 @@ function checkIfCharacterIsInString(text, character) {}
  *  isEven(11);
  *  //> false
  */
-function isEven(num) {}
+function isEven(num) {
+  return num%2 === 0;
+
+}
 
 /**
  * isOdd()
@@ -111,7 +129,9 @@ function isEven(num) {}
  *  isOdd(11);
  *  //> true
  */
-function isOdd(num) {}
+function isOdd(num) {
+  return num%2 === 1;
+}
 
 /**
  * isTruthy()
@@ -127,7 +147,14 @@ function isOdd(num) {}
  *  isTruthy(null);
  *  //> false
  */
-function isTruthy(val) {}
+function isTruthy(val) {
+ if (val === val){
+   return true;
+ } else (val === val)
+   return false;
+ }
+   
+
 
 /**
  * isFalsy()
