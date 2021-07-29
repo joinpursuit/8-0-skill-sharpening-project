1d5aa737f48dc41d5d0e0145b55501ba7faa8311
Taylor W <taylorwilliams@pursuit.org>
2021-07-28 23:12:28 -0400

    Answers to even-more-loops

--- src/07-even-more-loops.js
+++ src/07-even-more-loops.js
@@ -12,7 +12,20 @@
  *  shoutForLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForLoop(array) {}
+function shoutForLoop(array) {
+  // This function takes in array and returns a new array adding "1" to each string
+  // Declare a variable that holds a new array
+  let newArr = [];
+  // Create a loop that goes through each element of the array
+  for (let i = 0; i < array.length; i++) {
+    // Create an if statement that adds ! to each string and adds to new array
+    if (array[i]) {
+      array[i] = array[i] + "!";
+      newArr.push(array[i]);
+    }
+  }
+  return newArr;
+}
 
 /**
  * shoutWhileLoop()
@@ -28,7 +41,21 @@ function shoutForLoop(array) {}
  *  shoutWhileLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutWhileLoop(array) {}
+function shoutWhileLoop(array) {
+  // This function does the same as the above function but within a while loop
+  let newestArr = [];
+  let i = 0;
+  //Create a while loop
+  while (i < array.length) {
+    //if (arrray[i]) {
+    //array += '!'
+    newestArr.push(array[i] + "!");
+    i++;
+  }
+  return newestArr;
+}
+
+// This function takes in a new array and adds ! to each string
 
 /**
  * shoutForOfLoop()
@@ -44,7 +71,15 @@ function shoutWhileLoop(array) {}
  *  shoutForOfLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForOfLoop(array) {}
+function shoutForOfLoop(array) {
+  let emptyArr = [];
+  for (arr of array) {
+    //if (array[i]) {
+    // arr = array[i] + "!";
+    emptyArr.push(arr + "!");
+  }
+  return emptyArr;
+}
 
 /**
  * sumArray()
@@ -57,7 +92,16 @@ function shoutForOfLoop(array) {}
  *  sumArray([ 10, 0, 10, 11 ]);
  *  //> 31
  */
-function sumArray(array) {}
+function sumArray(array) {
+  // This function returns the sum of all values in the array
+  // Create a variable that holds a number
+  let num = 0;
+  // Create a for loop that loops through the array
+  for (let i = 0; i < array.length; i++) {
+    num += array[i];
+  }
+  return num;
+}
 
 /**
  * oddArray()
@@ -70,7 +114,17 @@ function sumArray(array) {}
  *  oddArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 11, 15, 37 ]
  */
-function oddArray(array) {}
+function oddArray(array) {
+  // Declare a variable that holds an empty array
+  let emptyArr = [];
+  // Create a for loop that loops through each element
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 == 1) {
+      emptyArr.push(array[i]);
+    }
+  }
+  return emptyArr;
+}
 
 /**
  * evenArray()
@@ -83,7 +137,17 @@ function oddArray(array) {}
  *  evenArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 20, 22 ]
  */
-function evenArray(array) {}
+function evenArray(array) {
+  // Declare a variable an assign it to an empty array
+  let newArr1 = [];
+  // Create a loop that goes through each element
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 == 0) {
+      newArr1.push(array[i]);
+    }
+  }
+  return newArr1;
+}
 
 /**
  * findSmallest()
@@ -96,7 +160,19 @@ function evenArray(array) {}
  *  findSmallest([ 0, 11, -2, 5 ]);
  *  //> -2
  */
-function findSmallest(array) {}
+function findSmallest(array) {
+  // Declare a variable that holds a number
+  let smallNum = 0;
+  // Declare a variable that holds the smallest number
+  let smallestNum = Math.min.apply(Math, array);
+  // Create a for loop that loops through each element in an array
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === smallestNum) {
+      smallNum = array[i];
+    }
+  }
+  return smallNum;
+}
 
 /**
  * findLargest()
@@ -109,7 +185,19 @@ function findSmallest(array) {}
  *  findLargest([ 0, 11, -2, 5 ]);
  *  //> 11
  */
-function findLargest(array) {}
+function findLargest(array) {
+  // Declare a variable that holds a number
+  let largeNum = 0;
+  // Declare a variable that holds the largest number
+  let largestNum = Math.max.apply(Math, array);
+  // Create a for loop that loops through each element in an array
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === largestNum) {
+      largeNum = array[i];
+    }
+  }
+  return largeNum;
+}
 
 /**
  * findEqual()
@@ -126,7 +214,18 @@ function findLargest(array) {}
  *  findEqual([ 0, 11, -2, 5 ], 9);
  *  //> false
  */
-function findEqual(array, selected) {}
+function findEqual(array, selected) {
+  // Declare a variable that holds a number
+  let selectedNum = false;
+  // Create a for loop that loops through each element
+  for (let i = 0; i < array.length; i++) {
+    // Create an if statement that finds the selected value
+    if (array[i] === selected) {
+      return true;
+    }
+  }
+  return selectedNum;
+}
 
 /**
  * removeDuplicates()
@@ -143,7 +242,10 @@ function findEqual(array, selected) {}
  *  //> [ 1, 11, 2, 3, 4, 9 ]
  */
 
-function removeDuplicates(array) {}
+function removeDuplicates(array) {
+  let oneElement = [...new Set(array)];
+  return oneElement;
+}
 
 // Do not change any code below this line.
 module.exports = {
