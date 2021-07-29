f2a52dc6b2ea21e153f51adf53a49a0ff0c83f84
Trevor Triumph <trevortriumph@Trevors-MBP.fios-router.home>
2021-07-29 09:30:39 -0400

    06

--- src/06-loops.js
+++ src/06-loops.js
@@ -11,7 +11,24 @@
  *  //> 25
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
-function rangeSum(min, max) {}
+function rangeSum(min, max) {
+// Declare a variable
+let totalSum = 0;
+// decide loop to use:
+  for (let i = min; i <= max; i++) {
+  totalSum = totalSum + i;
+  }
+  return totalSum;
+}
+// or:
+//let total = 0;
+//let current = min;
+// while (current <= max) {
+//total = total + current;
+//current = current + 1;
+//}
+//return total;
+//}
 
 /**
  * rangeOdd()
@@ -25,7 +42,15 @@ function rangeSum(min, max) {}
  *  rangeOdd(10, 15);
  *  //> [ 15, 13, 11 ]
  */
-function rangeOdd(min, max) {}
+function rangeOdd(min, max) {
+  let oddSum = [];
+  for (let i = min; i <= max; i++) {
+    if(i % 2 === 1) {
+      oddSum.unshift(i);
+    }
+  }
+  return oddSum;
+}
 
 /**
  * rangeEveryOther()
@@ -39,7 +64,23 @@ function rangeOdd(min, max) {}
  *  rangeEveryOther(11, 18);
  *  //> [ 11, 13, 15, 17 ]
  */
-function rangeEveryOther(min, max) {}
+function rangeEveryOther(min, max) {
+  let otherEle = [];
+  for (let i = min; i <= max; i+= 2) {
+    otherEle.push(i)
+  }
+  return otherEle;
+}
+
+// const result = [min];
+// let current = min + 2;
+// while (current <= max) {
+// result.push(current);
+// current = current + 2;
+//}
+
+// return result;
+
 
 /**
  * containsWhileLoop()
@@ -61,7 +102,30 @@ function rangeEveryOther(min, max) {}
  *  containsWhileLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsWhileLoop(array, target) {}
+function containsWhileLoop(array, target) {
+  let i = 0;
+  while (i < array.length) {
+    if (array[i]===target) {
+      return true;
+    }
+    i++;
+  }
+  return false
+}
+
+
+  //let targetEle = true;
+  //let counter = 0;
+ //while (targetEle) {
+  //  if (array.indexOf(target)) {
+    //  targetEle === false
+    //  counter ++
+  //  }
+  //}
+  
+  //return targetEle
+
+//console.log(containsWhileLoop([ 5, 2, 4 ], 4));
 
 /**
  * containsForLoop()
@@ -83,7 +147,16 @@ function containsWhileLoop(array, target) {}
  *  containsForLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsForLoop(array, target) {}
+function containsForLoop(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]===target) {
+      return true;
+    }
+  }
+  return false
+}
+
+
 
 /**
  * targetCount()
@@ -97,7 +170,15 @@ function containsForLoop(array, target) {}
  *  targetCount([ 10, 20, 10, 20, 30 ], 10);
  *  //> 2
  */
-function targetCount(array, target) {}
+function targetCount(array, target) {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (target === array[i]) {
+      count = count + 1
+    }
+  }
+  return count;
+}
 
 /**
  * firstIndexFound()
@@ -117,7 +198,14 @@ function targetCount(array, target) {}
  *  firstIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function firstIndexFound(array, target) {}
+function firstIndexFound(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if (target === array[i]) {
+      return i;
+    }
+  }
+  return -1;
+}
 
 /**
  * lastIndexFound()
@@ -137,7 +225,16 @@ function firstIndexFound(array, target) {}
  *  lastIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function lastIndexFound(array, target) {}
+function lastIndexFound(array, target) {
+  let targetFound = -1;
+  for (let i = 0; i < array.length; i++) {
+    if (target === array[i]) {
+     targetFound = i;
+    }
+  }
+  return targetFound;
+}
+
 
 /**
  * timesIndex()
@@ -151,8 +248,15 @@ function lastIndexFound(array, target) {}
  *  //> [ 7 * 0, 10 * 1, 11 * 2 ]
  *  //> [ 0, 10, 22 ]
  */
-function timesIndex(array) {}
-
+function timesIndex(array) {
+  let arr = array[0, array.length -1];
+  for (let i = 0; i < array.length; i++) {
+    if (arr * array.length);
+   
+  }
+  return arr;
+}
+timesIndex([ 7, 10, 11 ]);
 /**
  * cumulativeSum()
  * ---------------------
@@ -165,7 +269,16 @@ function timesIndex(array) {}
  *  //> [ 5, 5 + 2, 5 + 2 + 9 ]
  *  //> [ 5, 7, 16 ]
  */
-function cumulativeSum(array) {}
+function cumulativeSum(array) {
+  const result = [];
+  let total = 0;
+  for( let i = 0; i < array.length; i++){
+    total = total + array[i]
+    result.push(total);
+  }
+  return result;
+}
+
 
 // Do not change anything below this line.
 module.exports = {
