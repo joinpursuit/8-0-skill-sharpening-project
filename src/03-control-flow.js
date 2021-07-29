59750919e0cb9de2c4e22f1b9dd23a75b427a6a1
github username <github email address>
2021-07-29 10:05:54 -0400

    control flow completed

--- src/03-control-flow.js
+++ src/03-control-flow.js
@@ -14,7 +14,9 @@
  *  isEqual(10, "10");
  *  //> false
  */
-function isEqual(a, b) {}
+function isEqual(a, b) {
+return a === b
+}
 
 /**
  * findLarger()
@@ -28,7 +30,15 @@ function isEqual(a, b) {}
  *  findLarger(19, 7);
  *  //> 19
  */
-function findLarger(a, b) {}
+function findLarger(a, b) {
+//return Math.max(a, b)
+if (a > b) {
+  return a
+} 
+  else {
+  return b
+  }
+}
 
 /**
  * findLargerOrTie()
@@ -45,8 +55,25 @@ function findLarger(a, b) {}
  *  findLargerOrTie(0, 0);
  *  //> "tie"
  */
-function findLargerOrTie(a, b) {}
+// function findLargerOrTie(a, b) {
+//   if (a === b) {
+//   return 'tie'
+//   }
+//   else {
+// }
+//   return Math.max(a,b)
+// }
 
+function findLargerOrTie(a, b) {
+  if (a === b) {
+    return `tie`
+  }
+  if (a >= b) {
+    return a;
+  } else {
+    return b;
+  }
+}
 /**
  * positiveNegativeOrZero()
  * ---------------------
@@ -65,7 +92,28 @@ function findLargerOrTie(a, b) {}
  *  positiveNegativeOrZero(1);
  *  //> "Positive"
  */
-function positiveNegativeOrZero(a) {}
+function positiveNegativeOrZero(a) {
+  if (a > 0){
+    return "Positive"
+  }else if (a < 0) {
+    return "Negative"
+  } else {
+    return "Zero";
+   }
+  }
+
+//function positiveNegativeOrZero(a) {
+
+//   let value = "Put something" // If Input is empty, return an error message
+// if (a > 0) {
+//     value = "Positive"
+//   } else if ( a === 0) {
+//     value = "Zero"
+//   } else {
+//     value = "Negative"
+//   }
+//     return value
+// }
 
 /**
  * aroundTheWorldGreeting()
@@ -86,7 +134,15 @@ function positiveNegativeOrZero(a) {}
  *  aroundTheWorldGreeting();
  *  //> "Hello World"
  */
-function aroundTheWorldGreeting(language) {}
+function aroundTheWorldGreeting(language) {
+  if (language === "german"){
+    return "Hallo Welt";
+    } else if (language === "spanish") {
+    return "Hola Mundo";
+    } else {
+      return "Hello World"
+    }
+}
 
 /**
  * aroundTheWorldGreetingWithSwitch()
@@ -109,7 +165,18 @@ function aroundTheWorldGreeting(language) {}
  *  aroundTheWorldGreetingWithSwitch();
  *  //> "Hello World"
  */
-function aroundTheWorldGreetingWithSwitch(language) {}
+ function aroundTheWorldGreetingWithSwitch(language) {
+  switch (language) {
+    case 'spanish':
+      return `Hola Mundo`;
+      break;
+    case 'german':
+      return `Hallo Welt`
+      break;
+    default:
+      return `Hello World`
+  }
+}
 
 /**
  * calculateLetterGrade()
@@ -128,7 +195,44 @@ function aroundTheWorldGreetingWithSwitch(language) {}
  *  calculateLetterGrade(99);
  *  //> "A"
  */
-function calculateLetterGrade(grade) {}
+function calculateLetterGrade(grade) {
+  if (grade >= 90){
+    return 'A';
+    }else if (grade >= 80){
+     return 'B'
+    }else if (grade >= 70){
+      return 'C'
+    }else if (grade >= 60){
+      return 'D'
+    } else {
+      return 'F'
+    }
+}
+
+// if (grade.length === 0) {
+//   return null
+// }
+
+// let result = "";
+
+// if (grade >= 90) {
+//   result = "A";
+// }
+// else if (grade >= 80) {
+//   result = "B";
+// }
+// else if (grade >= 70) {
+//   result = "C";
+// }
+// else if (grade >= 60) {
+//   result = "D";
+// }
+// else {
+// result = "F"
+// }
+// return result
+
+// }
 
 /**
  * animalCounts()
@@ -146,7 +250,35 @@ function calculateLetterGrade(grade) {}
  *  animalCounts("pig", 1);
  *  //> "1 pig"
  */
-function animalCounts(animal, numberOfAnimals) {}
+function animalCounts(animal, numberOfAnimals) {
+//   if (numberOfAnimals>1){
+//     return `${numberOfAnimals} ${animal}s`
+//   }
+//   else {
+//   return `${numberOfAnimals} ${animal}`}
+// }
+return (numberOfAnimals > 1) ? `${numberOfAnimals} ${animal}s` : `${numberOfAnimals} ${animal}`;
+}
+
+// function animalCounts(animal, numberOfAnimals) {
+//   if (numberOfAnimals >= 2 ) {
+//     return `${numberOfAnimals} ${animal}s`
+//   } else {
+//     return `${numberOfAnimals} ${animal}`
+//   }
+// }
+
+// function animalCounts(animal, numberOfAnimals) {
+//   let countAnimal = " ";
+
+//   if (numberOfAnimals > 1) {
+//     countAnimal = `${numberOfAnimals} ${animal}s`;
+//   } 
+//   else { 
+//     countAnimal = `${numberOfAnimals} ${animal}`;
+//   }
+//   return countAnimal
+// }
 
 /**
  * evenOrOdd()
@@ -163,7 +295,16 @@ function animalCounts(animal, numberOfAnimals) {}
  *  evenOrOdd(48);
  *  //> "Even"
  */
-function evenOrOdd(a) {}
+function evenOrOdd(a) {
+//   if (a % 2 !== 0){
+//     return 'Odd'
+//   } else{
+//     return 'Even'
+//   }
+// }
+
+return (a % 2 !== 0) ? 'Odd' : 'Even'
+}
 
 /**
  * evenOrOddWithTernary()
@@ -179,7 +320,9 @@ function evenOrOdd(a) {}
  *  evenOrOddWithTernary(8);
  *  //> "Even"
  */
-function evenOrOddWithTernary(a) {}
+function evenOrOddWithTernary(a) {
+  return (a % 2 === 0) ? "Even" : "Odd";
+}
 
 // Do not change any code below this line.
 module.exports = {
