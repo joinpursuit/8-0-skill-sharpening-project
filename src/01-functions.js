70ee550765e1c42b11016a56c4d61b80431e3ba5
Trevor Triumph <trevortriumph@Trevors-MBP.fios-router.home>
2021-07-29 09:19:15 -0400

    01

--- src/01-functions.js
+++ src/01-functions.js
@@ -9,7 +9,9 @@
  *  alwaysTrue();
  *  //> true
  */
-function alwaysTrue() {}
+function alwaysTrue() {
+  return true;
+}
 
 /**
  * greet()
@@ -24,7 +26,9 @@ function alwaysTrue() {}
  *  greet("Ian");
  *  //> "Hello, Ian."
  */
-function greet() {}
+function greet(name) {
+  return `Hello, ${name}.`
+}
 
 /**
  * add()
@@ -39,7 +43,10 @@ function greet() {}
  *  add(10, 20);
  *  //> 30
  */
-function add() {}
+function add(a, b) {
+  let sum = a + b;
+  return sum;
+}
 
 /**
  * multiply()
@@ -54,7 +61,10 @@ function add() {}
  *  multiply(2, 5);
  *  //> 10
  */
-function multiply() {}
+function multiply(a, b) {
+  let product = a * b;
+  return product;
+}
 
 /**
  * average()
@@ -74,7 +84,12 @@ function multiply() {}
  *  average(10, 6);
  *  //> 8 // 10 + 6 = 16; 16/2 = 8;
  */
-function average() {}
+function average(a, b) {
+  let sum = a + b;
+  let average = (sum) / 2
+
+  return average;
+}
 
 /**
  * averageThree()
@@ -93,7 +108,12 @@ function average() {}
  *  averageThree(10, 11, 19);
  *  //> 10 // 10 + 11 + 19 = 30; 30 / 3 = 10;
  */
-function averageThree() {}
+function averageThree(a, b, c) {
+  let sum = a + b + c;
+  let average= (sum) / 3
+
+  return average;
+}
 
 /**
  * compareTypes()
@@ -112,7 +132,20 @@ function averageThree() {}
  *  compareTypes("left", 10);
  *  //> false
  */
-function compareTypes() {}
+//operators:
+// unary - take one operand
+// binary - take two operands
+// ternary -take three operands
+function compareTypes(a, b) {
+
+  if (typeof(a) === typeof(b)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// or:
+//return typeof (a) === typeof (b)
 
 /**
  * flipSign()
@@ -131,7 +164,11 @@ function compareTypes() {}
  *  flipSign(50);
  *  //> -50
  */
-function flipSign() {}
+function flipSign(a) {
+  if (a > 0 || a < 0) {
+    return a * -1;
+  }  
+}
 
 /**
  * joinStringsWithSpaces()
@@ -151,7 +188,11 @@ function flipSign() {}
  *  joinStringsWithSpaces("left", "right", "up", "down", "away");
  *  //> "left right up down away"
  */
-function joinStringsWithSpaces() {}
+function joinStringsWithSpaces(a, b, c, d, e) {
+  let joinTogether = `${a + ' ' + b + ' ' + c + ' ' + d + ' ' + e}`
+  return joinTogether;
+}
+   
 
 /**
  * getFirstAndLastCharacter()
@@ -168,7 +209,10 @@ function joinStringsWithSpaces() {}
  *  getFirstAndLastCharacter("upwards");
  *  //> "us"
  */
-function getFirstAndLastCharacter() {}
+function getFirstAndLastCharacter(a) { 
+  
+  return a[0].concat(a[a.length -1]);
+}
 
 // Do not change the code below.
 module.exports = {
