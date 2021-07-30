54b318cc0f2bdd5760e1b9043abbdc18b512ab37
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2021-07-30 08:06:59 -0400

    completed even more loops file

--- src/07-even-more-loops.js
+++ src/07-even-more-loops.js
@@ -12,7 +12,14 @@
  *  shoutForLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForLoop(array) {}
+function shoutForLoop(array) {
+  let newArr = [];
+
+  for (let i = 0; i < array.length; i++){
+    newArr.push(array[i] + "!");
+  }
+  return newArr;
+}
 
 /**
  * shoutWhileLoop()
@@ -28,7 +35,16 @@ function shoutForLoop(array) {}
  *  shoutWhileLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutWhileLoop(array) {}
+function shoutWhileLoop(array) {
+  const newArr = [];
+  let i = 0;
+
+  while (i < array.length) {
+    newArr.push(array[i] + "!");
+    i++;
+  }
+  return newArr;
+}
 
 /**
  * shoutForOfLoop()
@@ -44,7 +60,14 @@ function shoutWhileLoop(array) {}
  *  shoutForOfLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForOfLoop(array) {}
+function shoutForOfLoop(array) {
+  let newArr = [];
+
+  for (const eachElement of array){
+    newArr.push(eachElement + "!");
+  }
+  return newArr;
+}
 
 /**
  * sumArray()
@@ -57,7 +80,14 @@ function shoutForOfLoop(array) {}
  *  sumArray([ 10, 0, 10, 11 ]);
  *  //> 31
  */
-function sumArray(array) {}
+function sumArray(array) {
+  let sum = 0;
+
+  for (const eachNumber of array) {
+    sum += eachNumber;
+  }
+  return sum;
+}
 
 /**
  * oddArray()
@@ -70,7 +100,16 @@ function sumArray(array) {}
  *  oddArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 11, 15, 37 ]
  */
-function oddArray(array) {}
+function oddArray(array) {
+  let newArr = [];
+
+  for (const eachNumber of array) {
+    if (eachNumber % 2 !== 0) {
+      newArr.push(eachNumber);
+    }
+  }
+  return newArr;
+}
 
 /**
  * evenArray()
@@ -83,7 +122,16 @@ function oddArray(array) {}
  *  evenArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 20, 22 ]
  */
-function evenArray(array) {}
+function evenArray(array) {
+  let newArr = [];
+
+  for (const eachNumber of array) {
+    if (eachNumber % 2 === 0) {
+      newArr.push(eachNumber);
+    }
+  }
+  return newArr;
+}
 
 /**
  * findSmallest()
@@ -96,7 +144,16 @@ function evenArray(array) {}
  *  findSmallest([ 0, 11, -2, 5 ]);
  *  //> -2
  */
-function findSmallest(array) {}
+function findSmallest(array) {
+  let smallestNum = array[0];
+
+  for (const currentNum of array) {
+    if (currentNum < smallestNum) {
+      smallestNum = currentNum;
+    }
+  }
+  return smallestNum;
+}
 
 /**
  * findLargest()
@@ -109,7 +166,16 @@ function findSmallest(array) {}
  *  findLargest([ 0, 11, -2, 5 ]);
  *  //> 11
  */
-function findLargest(array) {}
+function findLargest(array) {
+  let largestNum = array[0];
+
+  for (const currentNum of array) {
+    if (currentNum > largestNum) {
+      largestNum = currentNum;
+    }
+  }
+  return largestNum;
+}
 
 /**
  * findEqual()
@@ -126,12 +192,22 @@ function findLargest(array) {}
  *  findEqual([ 0, 11, -2, 5 ], 9);
  *  //> false
  */
-function findEqual(array, selected) {}
+function findEqual(array, selected) {
+  let defaultVal = false;
+
+  for (const currentNum of array) {
+    if (currentNum === selected) {
+      defaultVal = true;
+    }
+  }
+  return defaultVal;
+}
 
 /**
  * removeDuplicates()
  * ---------------------
- * Returns a new array like the original array except there are no duplicates. The numbers in the array should be ordered similarly.
+ * Returns a new array like the original array except there are no duplicates. 
+ * The numbers in the array should be ordered similarly.
  *
  * HINT: The `.includes()` method could help here.
  *
@@ -143,7 +219,16 @@ function findEqual(array, selected) {}
  *  //> [ 1, 11, 2, 3, 4, 9 ]
  */
 
-function removeDuplicates(array) {}
+function removeDuplicates(array) {
+  const newArr = [];
+
+  for (const currentNum of array) {
+    if (!newArr.includes(currentNum)) {
+      newArr.push(currentNum);
+    }
+  }
+  return newArr;
+}
 
 // Do not change any code below this line.
 module.exports = {
