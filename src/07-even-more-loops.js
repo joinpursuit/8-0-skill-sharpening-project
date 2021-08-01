290dd3977dc52dd2308da1b2ff84e39cc089de73
rob <robwalker@pursuit.org>
2021-08-01 13:07:15 -0400

    skills-more-loops-questions

--- src/07-even-more-loops.js
+++ src/07-even-more-loops.js
@@ -12,8 +12,13 @@
  *  shoutForLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForLoop(array) {}
-
+function shoutForLoop(array) {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    newArr.push(array[i]+ '!');
+  }
+  return newArr;
+}
 /**
  * shoutWhileLoop()
  * ---------------------
@@ -28,8 +33,15 @@ function shoutForLoop(array) {}
  *  shoutWhileLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutWhileLoop(array) {}
-
+function shoutWhileLoop(array) {
+  let newArr = [];
+  let i = 0;
+  while (i < array.length) {
+    newArr.push(array[i] + '!');
+    i++;
+  }
+  return newArr;
+}
 /**
  * shoutForOfLoop()
  * ---------------------
@@ -44,7 +56,13 @@ function shoutWhileLoop(array) {}
  *  shoutForOfLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForOfLoop(array) {}
+function shoutForOfLoop(array) {
+  let newArr = [];
+  for(let arr of array) {
+    newArr.push(arr + '!');
+  }
+  return newArr;
+}
 
 /**
  * sumArray()
@@ -57,7 +75,13 @@ function shoutForOfLoop(array) {}
  *  sumArray([ 10, 0, 10, 11 ]);
  *  //> 31
  */
-function sumArray(array) {}
+function sumArray(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+     sum += array[i];
+  }
+  return sum;
+}
 
 /**
  * oddArray()
@@ -70,7 +94,15 @@ function sumArray(array) {}
  *  oddArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 11, 15, 37 ]
  */
-function oddArray(array) {}
+function oddArray(array) {
+  let arrOdd = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 1) {
+      arrOdd.push(array[i]);
+    }
+  }
+  return arrOdd;
+}
 
 /**
  * evenArray()
@@ -83,7 +115,15 @@ function oddArray(array) {}
  *  evenArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 20, 22 ]
  */
-function evenArray(array) {}
+function evenArray(array) {
+  let arrEvn = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      arrEvn.push(array[i]);
+    }
+  }
+  return arrEvn;
+}
 
 /**
  * findSmallest()
@@ -96,7 +136,15 @@ function evenArray(array) {}
  *  findSmallest([ 0, 11, -2, 5 ]);
  *  //> -2
  */
-function findSmallest(array) {}
+function findSmallest(array) {
+  let smNum = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < smNum) {
+      smNum = array[i];
+    }
+  }
+  return smNum;
+}
 
 /**
  * findLargest()
@@ -109,7 +157,15 @@ function findSmallest(array) {}
  *  findLargest([ 0, 11, -2, 5 ]);
  *  //> 11
  */
-function findLargest(array) {}
+function findLargest(array) {
+  let lgNum = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > lgNum) {
+      lgNum = array[i];
+    }
+  }
+  return lgNum;
+}
 
 /**
  * findEqual()
@@ -126,7 +182,15 @@ function findLargest(array) {}
  *  findEqual([ 0, 11, -2, 5 ], 9);
  *  //> false
  */
-function findEqual(array, selected) {}
+function findEqual(array, selected) {
+  let eqNum = false;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === selected) {
+     eqNum = true;
+    }
+  }
+  return eqNum;
+}
 
 /**
  * removeDuplicates()
@@ -143,7 +207,15 @@ function findEqual(array, selected) {}
  *  //> [ 1, 11, 2, 3, 4, 9 ]
  */
 
-function removeDuplicates(array) {}
+function removeDuplicates(array) {
+  let dupArrNum = [];
+  for (let i = 0; i < array.length; i++) {
+    if(!dupArrNum.includes(array[i])) {
+      dupArrNum.push(array[i]);
+    }
+  }
+  return dupArrNum;
+}
 
 // Do not change any code below this line.
 module.exports = {
