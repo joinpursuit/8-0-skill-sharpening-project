f254ceb746cf23f2517e39c11705dc9dd8793093
Jonathan Scheiber <jon@scheiber.me>
2022-01-26 13:06:35 -0500

    complete 01-functions.js

--- src/01-functions.js
+++ src/01-functions.js
@@ -9,7 +9,10 @@
  *  alwaysTrue();
  *  //> true
  */
-function alwaysTrue() {}
+function alwaysTrue() {
+  // return true boolean no matter what
+  return true;
+}
 
 /**
  * greet()
@@ -24,7 +27,10 @@ function alwaysTrue() {}
  *  greet("Ian");
  *  //> "Hello, Ian."
  */
-function greet() {}
+function greet(name) {
+  // return name parameter as string using templete literal
+  return `Hello, ${name}.`;
+}
 
 /**
  * add()
@@ -39,7 +45,10 @@ function greet() {}
  *  add(10, 20);
  *  //> 30
  */
-function add() {}
+function add(a, b) {
+  // return the sum of variables a and b
+  return a + b;
+}
 
 /**
  * multiply()
@@ -54,7 +63,10 @@ function add() {}
  *  multiply(2, 5);
  *  //> 10
  */
-function multiply() {}
+function multiply(a ,b) {
+  // return the product of variables a and b
+  return a * b;
+}
 
 /**
  * average()
@@ -74,7 +86,10 @@ function multiply() {}
  *  average(10, 6);
  *  //> 8 // 10 + 6 = 16; 16/2 = 8;
  */
-function average() {}
+function average(a , b) {
+  // add a and b together and divide by 2 and return average
+  return (a + b) / 2;
+}
 
 /**
  * averageThree()
@@ -93,7 +108,10 @@ function average() {}
  *  averageThree(10, 11, 19);
  *  //> 10 // 10 + 11 + 19 = 30; 30 / 3 = 10;
  */
-function averageThree() {}
+function averageThree(a, b, c) {
+  // add a, b, and c together and divide by 3 to return average
+  return (a + b + c) / 3;
+}
 
 /**
  * compareTypes()
@@ -112,7 +130,16 @@ function averageThree() {}
  *  compareTypes("left", 10);
  *  //> false
  */
-function compareTypes() {}
+function compareTypes(a, b) {
+  // use typeof operator to check if a and b are same type.
+  // if so, return true; otherwise, return false.
+  if (typeof a === typeof b) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 /**
  * flipSign()
@@ -131,7 +158,10 @@ function compareTypes() {}
  *  flipSign(50);
  *  //> -50
  */
-function flipSign() {}
+function flipSign(a) {
+  // use negative sign to make postive number negative or negative number positive
+  return -a;
+}
 
 /**
  * joinStringsWithSpaces()
@@ -151,7 +181,10 @@ function flipSign() {}
  *  joinStringsWithSpaces("left", "right", "up", "down", "away");
  *  //> "left right up down away"
  */
-function joinStringsWithSpaces() {}
+function joinStringsWithSpaces(a, b, c, d, e) {
+  // use template literals to combine five strings with spaces
+  return `${a} ${b} ${c} ${d} ${e}`;
+}
 
 /**
  * getFirstAndLastCharacter()
@@ -168,7 +201,10 @@ function joinStringsWithSpaces() {}
  *  getFirstAndLastCharacter("upwards");
  *  //> "us"
  */
-function getFirstAndLastCharacter() {}
+function getFirstAndLastCharacter(a) {
+  // return start of string with charAt() and end of string with slice()
+  return a.charAt(0) + a.slice(-1);
+}
 
 // Do not change the code below.
 module.exports = {
