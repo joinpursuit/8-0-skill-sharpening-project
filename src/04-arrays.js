d35bec8439c5c16aa11e27be1a906a9909040931
lily08 <liliangelxd@gmail.com>
2022-01-26 15:23:51 -0500

    Finished Array

--- src/04-arrays.js
+++ src/04-arrays.js
@@ -10,7 +10,13 @@
  *  createEmptyArray();
  *  //> []
  */
-function createEmptyArray() {}
+function createEmptyArray() {
+    
+  return []
+
+
+
+}
 
 /**
  * createArrayWithTwoElements()
@@ -24,7 +30,12 @@ function createEmptyArray() {}
  *  createArrayWithTwoElements(true, false);
  *  //> [ true, false ]
  */
-function createArrayWithTwoElements(a, b) {}
+function createArrayWithTwoElements(a, b) {
+    let array = [a,b]
+  return array
+
+
+}
 
 /**
  * getArrayLength()
@@ -37,7 +48,10 @@ function createArrayWithTwoElements(a, b) {}
  *  getArrayLength([ 10, 20, 30 ]);
  *  //> 3
  */
-function getArrayLength(array) {}
+function getArrayLength(array) {
+        return  array.length
+
+}
 
 /**
  * getFirstElementOfArray()
@@ -51,7 +65,11 @@ function getArrayLength(array) {}
  *  getFirstElementOfArray([ 10, 20, 30 ]);
  *  //> 10
  */
-function getFirstElementOfArray(array) {}
+function getFirstElementOfArray(array) {
+      
+        return array[0]
+
+}
 
 /**
  * getLastElementOfArray()
@@ -65,7 +83,11 @@ function getFirstElementOfArray(array) {}
  *  getLastElementOfArray([ null, undefined ]);
  *  //> undefined
  */
-function getLastElementOfArray(array) {}
+function getLastElementOfArray(array) {
+
+  
+  return array[array.length-1]
+}
 
 /**
  * addElementToEndOfArray()
@@ -79,7 +101,12 @@ function getLastElementOfArray(array) {}
  *  addElementToEndOfArray([ 10 ], 9);
  *  //> [ 10, 9 ]
  */
-function addElementToEndOfArray(array, element) {}
+function addElementToEndOfArray(array, element) {
+  array.push(element)
+  return  array
+
+
+}
 
 /**
  * removeElementFromEndOfArray()
@@ -92,7 +119,11 @@ function addElementToEndOfArray(array, element) {}
  *  removeElementFromEndOfArray([ 10, 9, 8 ]);
  *  //> 8
  */
-function removeElementFromEndOfArray(array) {}
+function removeElementFromEndOfArray(array) {
+
+    return  array.pop()
+
+}
 
 /**
  * addElementToFrontOfArray()
@@ -106,7 +137,13 @@ function removeElementFromEndOfArray(array) {}
  *  addElementToFrontOfArray([ 10 ], 9);
  *  //> [ 9, 10 ]
  */
-function addElementToFrontOfArray(array, element) {}
+function addElementToFrontOfArray(array, element) {
+
+     array.unshift(element)
+    return array
+
+
+}
 
 /**
  * removeElementFromFrontOfArray()
@@ -119,7 +156,10 @@ function addElementToFrontOfArray(array, element) {}
  *  removeElementFromFrontOfArray([ 10, 9, 8 ]);
  *  //> 10
  */
-function removeElementFromFrontOfArray(array) {}
+function removeElementFromFrontOfArray(array) {
+       return  array.shift()
+
+}
 
 /**
  * getMiddleElement()
@@ -135,7 +175,11 @@ function removeElementFromFrontOfArray(array) {}
  *  getMiddleElement([ 10, null, "30" ]);
  *  //> null
  */
-function getMiddleElement(array) {}
+function getMiddleElement(array) {
+  let middle = Math.floor(array.length / 2)
+      return array[middle]
+
+}
 
 // Do not change any code below this line.
 module.exports = {
