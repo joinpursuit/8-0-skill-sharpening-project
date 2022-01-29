81e48919f6dac31c781bc1cddb8d40187e8f0381
Michael Kleemoff <michaelkleemoff2@gmail.com>
2022-01-29 15:31:27 -0500

    Pass final test isFalsy() in 02-data-types.js

--- src/02-data-types.js
+++ src/02-data-types.js
@@ -175,7 +175,14 @@ function isTruthy(val) {
  *  isFalsy(" ");
  *  //> false
  */
-function isFalsy(val) {}
+function isFalsy(val) {
+  if (!val) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
 
 // Do not change the code below.
 module.exports = {
