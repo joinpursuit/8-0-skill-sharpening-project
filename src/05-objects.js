32081dd27be71bbd2dbdcc41c2f069f97fe63a9d
Michael Kleemoff <michaelkleemoff2@gmail.com>
2022-01-30 10:37:15 -0500

    Pass addKeyToObject() in 05-objects.js

--- src/05-objects.js
+++ src/05-objects.js
@@ -123,7 +123,10 @@ function addValueToObject(object, val) {
  *  addKeyToObject({ right: false }, "left");
  *  //> { right: false, left: true }
  */
-function addKeyToObject(object, key) {}
+function addKeyToObject(object, key) {
+  object[key] = 'Michael';
+  return object;
+}
 
 /**
  * addKeyValuePairToObject()
