0b97cdbaf57e3d58c83a6a38a6cd1288b33af588
Jorge Carrera <JCarrera994@gmail.com>
2022-01-29 21:45:04 -0500

    08-accessing complete

--- src/08-accessing.js
+++ src/08-accessing.js
@@ -190,7 +190,18 @@ function getFullAddress(person) {
     };
  */
 function getFlatObject(person) {
-      //  WLL COME BACK TO 
+  let newObj = {
+    first : person.names.first,
+    middle : person.names.middle,
+    last : person.names.last,
+    street : person.address.street,
+    unit : person.address.unit,
+    city : person.address.city,
+    state : person.address.state,
+    zip : person.address.zip,
+    numbers : person.numbers
+  }
+  return newObj
 }
 
 // Do not change the code below.
