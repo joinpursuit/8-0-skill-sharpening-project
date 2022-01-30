609e04c4ebc8e66ac36b1a9241e59113cf97531b
T.K <nique@niques-air.myfiosgateway.com>
2022-01-30 16:33:43 -0500

    pass 5 loops test

--- src/06-loops.js
+++ src/06-loops.js
@@ -11,7 +11,13 @@
  *  //> 25
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
-function rangeSum(min, max) {}
+function rangeSum(min, max) {
+  let sum = 0;
+  for (let i = min; i <= max; i++){
+    sum += i;
+  };
+  return sum;
+};
 
 /**
  * rangeOdd()
@@ -25,8 +31,15 @@ function rangeSum(min, max) {}
  *  rangeOdd(10, 15);
  *  //> [ 15, 13, 11 ]
  */
-function rangeOdd(min, max) {}
-
+function rangeOdd(min, max) {
+  let arr = [];
+  for (let i = max; i >= min; i--){
+    if (i % 2 !== 0){
+      arr.push(i);
+    }
+  }
+  return arr;
+};
 /**
  * rangeEveryOther()
  * ---------------------
@@ -39,7 +52,13 @@ function rangeOdd(min, max) {}
  *  rangeEveryOther(11, 18);
  *  //> [ 11, 13, 15, 17 ]
  */
-function rangeEveryOther(min, max) {}
+function rangeEveryOther(min, max) {
+  let arr = [];
+  for (let i = min; i <= max; i += 2){
+    arr.push(i);
+  }
+  return arr;
+};
 
 /**
  * containsWhileLoop()
