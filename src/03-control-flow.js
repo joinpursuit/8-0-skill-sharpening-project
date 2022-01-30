879edf19360ddf1c470b0012976380db19113604
Michael Kleemoff <michaelkleemoff2@gmail.com>
2022-01-29 19:27:42 -0500

    Pass aroundTheWorldGreeting() in 03-control-flow.js

--- src/03-control-flow.js
+++ src/03-control-flow.js
@@ -118,7 +118,15 @@ function positiveNegativeOrZero(a) {
  *  aroundTheWorldGreeting();
  *  //> "Hello World"
  */
-function aroundTheWorldGreeting(language) {}
+ function aroundTheWorldGreeting(language = 'english') {
+  if (language.toLowerCase() === 'english') {
+    return 'Hello World';
+  } else if (language.toLowerCase() === 'spanish') {
+    return 'Hola Mundo';
+  } else if (language.toLowerCase() === 'german') {
+    return 'Hallo Welt';
+  }
+}
 
 /**
  * aroundTheWorldGreetingWithSwitch()
