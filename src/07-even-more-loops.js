1c9fda2fb858d07f4c5c4d045dff679017fc8799
Jorge Carrera <JCarrera994@gmail.com>
2022-01-29 21:38:30 -0500

    07-more-loops complete

--- src/07-even-more-loops.js
+++ src/07-even-more-loops.js
@@ -63,7 +63,13 @@ function shoutWhileLoop(array) {
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
 function shoutForOfLoop(array) {
-  
+let newArr =[];
+let newEle;
+  for (let i of array){
+  newEle = [i] + "!"
+  newArr.push(newEle)
+}
+return newArr
 }
 
 /**
@@ -77,7 +83,13 @@ function shoutForOfLoop(array) {
  *  sumArray([ 10, 0, 10, 11 ]);
  *  //> 31
  */
-function sumArray(array) {}
+function sumArray(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++){
+    sum += array[i]
+  }
+  return sum
+}
 
 /**
  * oddArray()
@@ -90,7 +102,14 @@ function sumArray(array) {}
  *  oddArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 11, 15, 37 ]
  */
-function oddArray(array) {}
+function oddArray(array) {
+  let newArr = [];
+  for (i=0; i < array.length; i++){
+    if(array[i] % 2 !== 0){
+   newArr.push(array[i])
+    }
+  }return newArr
+}
 
 /**
  * evenArray()
@@ -103,7 +122,14 @@ function oddArray(array) {}
  *  evenArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 20, 22 ]
  */
-function evenArray(array) {}
+function evenArray(array) {
+  let newArr = []
+  for (let i = 0; i < array.length; i++){
+    if(array[i] % 2 === 0){
+      newArr.push(array[i])
+    }
+  }return newArr
+}
 
 /**
  * findSmallest()
@@ -116,7 +142,14 @@ function evenArray(array) {}
  *  findSmallest([ 0, 11, -2, 5 ]);
  *  //> -2
  */
-function findSmallest(array) {}
+function findSmallest(array) {
+  let smallest = array[0]
+  for (let i = 0; i < array.length; i++){
+    if (smallest > array[i]){
+      smallest = array[i]
+    }
+  }return smallest
+}
 
 /**
  * findLargest()
@@ -129,7 +162,15 @@ function findSmallest(array) {}
  *  findLargest([ 0, 11, -2, 5 ]);
  *  //> 11
  */
-function findLargest(array) {}
+function findLargest(array) {
+let largest = array[0];
+  for (let i = 0; i < array.length; i++){
+    if(largest < array[i]){
+      largest = array[i]
+    }
+  }
+  return largest 
+}
 
 /**
  * findEqual()
@@ -146,7 +187,13 @@ function findLargest(array) {}
  *  findEqual([ 0, 11, -2, 5 ], 9);
  *  //> false
  */
-function findEqual(array, selected) {}
+function findEqual(array, selected) {
+  for (let i = 0; i < array.length; i++){
+    if (array[i] === selected){
+      return true 
+    }
+  }return false 
+}
 
 /**
  * removeDuplicates()
@@ -163,7 +210,15 @@ function findEqual(array, selected) {}
  *  //> [ 1, 11, 2, 3, 4, 9 ]
  */
 
-function removeDuplicates(array) {}
+function removeDuplicates(array) {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++){
+    if(!newArr.includes(array[i])){
+      newArr.push(array[i])
+    }
+  }
+  return newArr
+}
 
 // Do not change any code below this line.
 module.exports = {
