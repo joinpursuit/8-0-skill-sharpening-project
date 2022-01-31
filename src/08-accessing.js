212a0548b8e768b975e5353346660233f9c0c08e
Michael Kleemoff <michaelkleemoff2@gmail.com>
2022-01-30 21:59:54 -0500

    Pass getLastName(), getAddressStreet() in 08-accessing.js

--- src/08-accessing.js
+++ src/08-accessing.js
@@ -47,7 +47,9 @@ function getFirstName(person) {
  *  getLastName(person);
  *  //> "Rojas"
  */
-function getLastName(person) {}
+function getLastName(person) {
+  return `${person.names['last']}`;
+}
 
 /**
  * getAddressStreet()
@@ -60,7 +62,9 @@ function getLastName(person) {}
  *  getAddressStreet(person);
  *  //> "697 Pine Drive"
  */
-function getAddressStreet(person) {}
+function getAddressStreet(person) {
+  return `${person.address['street']}`;
+}
 
 /**
  * getCountOfPhoneNumbers()
