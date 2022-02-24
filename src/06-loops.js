7990942e673ed0676fa7e7485e6d59053059ac8c
KenyaMighty <kenyamighty@pursuit.org>
2022-02-23 19:23:50 -0500

    new sharp

--- src/06-loops.js
+++ src/06-loops.js
@@ -1,3 +1,5 @@
+const { add } = require("./01-functions");
+
 /**
  * rangeSum()
  * ---------------------
@@ -11,7 +13,13 @@
  *  //> 25
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
-function rangeSum(min, max) {}
+function rangeSum(min, max) {
+  let sum = 0;
+  for(let i = min; i <= max; i++){
+    sum += i;
+  }
+  return sum;
+}
 
 /**
  * rangeOdd()
@@ -25,7 +33,15 @@ function rangeSum(min, max) {}
  *  rangeOdd(10, 15);
  *  //> [ 15, 13, 11 ]
  */
-function rangeOdd(min, max) {}
+function rangeOdd(min, max) {
+  let oddUp = [];
+  for(let i = max; i >= min; i--){
+    if(i % 2 === 0){
+      oddUp.push(i);
+    }
+  }
+  return oddUp;
+}
 
 /**
  * rangeEveryOther()
@@ -39,7 +55,13 @@ function rangeOdd(min, max) {}
  *  rangeEveryOther(11, 18);
  *  //> [ 11, 13, 15, 17 ]
  */
-function rangeEveryOther(min, max) {}
+function rangeEveryOther(min, max) {
+  let rangeOfOther = [];
+  for(let i = min; i <= max; i+=2){
+    rangeOfOther.push(i);
+  }
+  return rangeOfOther;
+}
 
 /**
  * containsWhileLoop()
@@ -61,7 +83,11 @@ function rangeEveryOther(min, max) {}
  *  containsWhileLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsWhileLoop(array, target) {}
+function containsWhileLoop(array, target) {
+  while (array.indexOf(target) >= 0){
+    return true;
+  }
+}
 
 /**
  * containsForLoop()
@@ -83,7 +109,17 @@ function containsWhileLoop(array, target) {}
  *  containsForLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsForLoop(array, target) {}
+function containsForLoop(array, target) {
+  let i = 0;
+
+  while (i < array.length){
+    if (array[i] === target){
+      return true;
+    };
+    i++;
+  }
+  return false;
+}
 
 /**
  * targetCount()
@@ -97,7 +133,16 @@ function containsForLoop(array, target) {}
  *  targetCount([ 10, 20, 10, 20, 30 ], 10);
  *  //> 2
  */
-function targetCount(array, target) {}
+function targetCount(array, target) {
+  let addTo = 0;
+
+  for (let i = 0; i < array.length; i++){
+    if (array[i] === target){
+      addTo += 1;
+    };
+  };
+  return addTo;
+}
 
 /**
  * firstIndexFound()
@@ -117,7 +162,14 @@ function targetCount(array, target) {}
  *  firstIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function firstIndexFound(array, target) {}
+function firstIndexFound(array, target) {
+  for (let i = 0; i < array.lastIndexOf; i++){
+    if (array[i] === target){
+      return i;
+    }
+  }
+  return -1;
+}
 
 /**
  * lastIndexFound()
@@ -137,7 +189,14 @@ function firstIndexFound(array, target) {}
  *  lastIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function lastIndexFound(array, target) {}
+function lastIndexFound(array, target) {
+  for (let i = array.length; i >= 0; i--){
+    if (array[i] === target){
+      return i
+    }
+  }
+  return -1;
+}
 
 /**
  * timesIndex()
@@ -151,7 +210,14 @@ function lastIndexFound(array, target) {}
  *  //> [ 7 * 0, 10 * 1, 11 * 2 ]
  *  //> [ 0, 10, 22 ]
  */
-function timesIndex(array) {}
+function timesIndex(array) {
+  let multipliedNum = [];
+
+  for (let i = 0; i > array.length; i++){
+    multipliedNum.push(array[i] * i)
+  }
+  return multipliedNum;
+}
 
 /**
  * cumulativeSum()
@@ -165,7 +231,13 @@ function timesIndex(array) {}
  *  //> [ 5, 5 + 2, 5 + 2 + 9 ]
  *  //> [ 5, 7, 16 ]
  */
-function cumulativeSum(array) {}
+function cumulativeSum(array) {
+  let sumNums = [array[0]];
+  for (let i = 1; i < array.length; i++){
+    sumNums.push(sumNums[i - 1] + array[i]);
+  };
+  return sumNums
+}
 
 // Do not change anything below this line.
 module.exports = {
