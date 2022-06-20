e782573807efead5f8bc4df3aceaf05d2005502a
minagrullon <minagrullon@pursuit.org>
2022-06-20 14:29:47 -0400

    Progress

--- src/06-loops.js
+++ src/06-loops.js
@@ -11,7 +11,12 @@
  *  //> 25
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
-function rangeSum(min, max) {}
+function rangeSum(min, max) {
+  let sum = 0;
+  for(let i = min; i <= max; i++){
+sum += i
+  } return sum
+}
 
 /**
  * rangeOdd()
@@ -25,7 +30,12 @@ function rangeSum(min, max) {}
  *  rangeOdd(10, 15);
  *  //> [ 15, 13, 11 ]
  */
-function rangeOdd(min, max) {}
+function rangeOdd(min, max) {
+  let odd = [];
+  for(let i = min; i <= max; i++){
+    
+  }
+}
 
 /**
  * rangeEveryOther()
