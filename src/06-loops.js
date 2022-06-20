01770fa55149d723116c35d0c7d3b4dc2a229588
Gillesedgy <edgygilles@pursuit.org>
2022-06-20 18:26:55 -0400

    I fixedLoops

--- src/06-loops.js
+++ src/06-loops.js
@@ -12,7 +12,11 @@
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
 function rangeSum(min, max) {
-  
+  let sum=0;
+  for (let i= min; i<= max; i++){
+    sum+=i
+  }
+  return sum
 }
 
 /**
@@ -27,7 +31,11 @@ function rangeSum(min, max) {
  *  rangeOdd(10, 15);
  *  //> [ 15, 13, 11 ]
  */
-function rangeOdd(min, max) {}
+function rangeOdd(min, max) {
+
+
+  
+}
 
 /**
  * rangeEveryOther()
