50abbae1d1f69ea60cb76fadba23e96fda0698a8
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-06-21 13:33:25 -0400

    some loops and accessing

--- src/06-loops.js
+++ src/06-loops.js
@@ -11,7 +11,13 @@
  *  //> 25
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
-function rangeSum(min, max) {}
+function rangeSum(min, max) {
+  let sum = 0;
+  for (let i = min;i <= max; i++){
+    sum += i
+  }
+  return sum;
+}
 
 /**
  * rangeOdd()
@@ -25,7 +31,15 @@ function rangeSum(min, max) {}
  *  rangeOdd(10, 15);
  *  //> [ 15, 13, 11 ]
  */
-function rangeOdd(min, max) {}
+function rangeOdd(min, max) {
+let arr = []
+  for (let i = max; i >= min; i--){
+if ( i % 2 === 1){
+  arr.push(i)
+}
+  }
+  return arr;
+}
 
 /**
  * rangeEveryOther()
@@ -39,7 +53,13 @@ function rangeOdd(min, max) {}
  *  rangeEveryOther(11, 18);
  *  //> [ 11, 13, 15, 17 ]
  */
-function rangeEveryOther(min, max) {}
+function rangeEveryOther(min, max) {
+  let arr = []
+  for (let i = min; i<= max; i += 2){
+    arr.push(i)
+  }
+  return arr;
+}
 
 /**
  * containsWhileLoop()
@@ -61,7 +81,17 @@ function rangeEveryOther(min, max) {}
  *  containsWhileLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsWhileLoop(array, target) {}
+function containsWhileLoop(array, target) {
+let i = 0;
+  while (array[i]){
+    if (array[i] !== target){
+      i++
+    }else {
+      return true
+    }
+  }
+  return false;
+}
 
 /**
  * containsForLoop()
@@ -83,7 +113,17 @@ function containsWhileLoop(array, target) {}
  *  containsForLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsForLoop(array, target) {}
+function containsForLoop(array, target) {
+ 
+  for (let i = 0; i <array.length; i++){
+    if (array[i] !== target){
+      continue;
+    }else {
+      return true;
+    }
+  }
+  return false;
+}
 
 /**
  * targetCount()
