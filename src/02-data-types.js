c8ff480d4f3f8e5bbaa2056e7f7d5efb819d2d03
SasukeDaTony <tonytaylor@pursuit.org>
2022-06-22 22:17:35 -0400

    completed-data-types

--- src/02-data-types.js
+++ src/02-data-types.js
@@ -9,7 +9,10 @@
  *  getLengthOfString("down");
  *  //> 4
  */
-function getLengthOfString(str) {}
+function getLengthOfString(str) {
+  strLength = str.length ;
+  return strLength ;
+}
 
 /**
  * convertToNumber()
@@ -22,7 +25,9 @@ function getLengthOfString(str) {}
  *  convertToNumber("111");
  *  //> 111
  */
-function convertToNumber(val) {}
+function convertToNumber(val) {
+  return ~~val ;
+}
 
 /**
  * convertToString()
@@ -36,7 +41,9 @@ function convertToNumber(val) {}
  *  convertToString(99);
  *  //> "99"
  */
-function convertToString(val) {}
+function convertToString(val) {
+  return `${val}`
+}
 
 /**
  * convertToShoutingText()
@@ -49,7 +56,9 @@ function convertToString(val) {}
  *  convertToShoutingText("Hello There");
  *  //> "HELLO THERE"
  */
-function convertToShoutingText(text) {}
+function convertToShoutingText(text) {
+  return text.toUpperCase() ; 
+}
 
 /**
  * convertToWhisperText()
@@ -62,7 +71,9 @@ function convertToShoutingText(text) {}
  *  convertToWhisperText("Hello There");
  *  //> "hello there"
  */
-function convertToWhisperText(text) {}
+function convertToWhisperText(text) {
+  return text.toLowerCase() ;
+}
 
 /**
  * checkIfCharacterIsInString()
@@ -79,7 +90,11 @@ function convertToWhisperText(text) {}
  *  checkIfCharacterIsInString("hello there", "a");
  *  //> false
  */
-function checkIfCharacterIsInString(text, character) {}
+function checkIfCharacterIsInString(text, character) {
+  if (text.includes(character)) {
+    return true;
+  } else return false ;
+};
 
 /**
  * isEven()
@@ -95,7 +110,11 @@ function checkIfCharacterIsInString(text, character) {}
  *  isEven(11);
  *  //> false
  */
-function isEven(num) {}
+function isEven(num) {
+  if (num%2 ===0 ) {
+    return true ;
+  } else return false ;
+}
 
 /**
  * isOdd()
@@ -111,7 +130,11 @@ function isEven(num) {}
  *  isOdd(11);
  *  //> true
  */
-function isOdd(num) {}
+function isOdd(num) {
+  if (num%2 !==0) {
+    return true ;
+  } else return false ;
+};
 
 /**
  * isTruthy()
@@ -127,7 +150,11 @@ function isOdd(num) {}
  *  isTruthy(null);
  *  //> false
  */
-function isTruthy(val) {}
+function isTruthy(val) {
+  if (val) {
+    return true ;
+  } else return false ;
+}
 
 /**
  * isFalsy()
@@ -143,7 +170,11 @@ function isTruthy(val) {}
  *  isFalsy(" ");
  *  //> false
  */
-function isFalsy(val) {}
+function isFalsy(val) {
+  if (!val) {
+    return true ;
+  } else return false ;
+}
 
 // Do not change the code below.
 module.exports = {
