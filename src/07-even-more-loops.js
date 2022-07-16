5e7ee190db70b460e4605d9d3212c66f8331a99b
ayd823 <aydiaz823@gmail.com>
2022-07-16 18:57:56 -0400

    Updated all files in src, completing each task.

--- src/07-even-more-loops.js
+++ src/07-even-more-loops.js
@@ -12,7 +12,19 @@
  *  shoutForLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForLoop(array) {}
+function shoutForLoop(array) {
+let shouts = [];
+
+for (let i = 0; i < array.length; i++) {
+  shouts.push(array[i] + "!");
+}
+
+return shouts;
+}
+
+//Alt method:
+
+//return array.map(value => value + "!");
 
 /**
  * shoutWhileLoop()
@@ -28,7 +40,16 @@ function shoutForLoop(array) {}
  *  shoutWhileLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutWhileLoop(array) {}
+function shoutWhileLoop(array) {
+  let shouts = [];
+  let i = 0;
+
+  while(i < array.length) {
+    shouts.push(array[i] + "!");
+    i++;
+  }
+  return shouts;
+}
 
 /**
  * shoutForOfLoop()
@@ -44,7 +65,14 @@ function shoutWhileLoop(array) {}
  *  shoutForOfLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForOfLoop(array) {}
+function shoutForOfLoop(array) {
+  let shouts = [];
+  
+  for (let value of array) {
+  shouts.push(value + "!");
+}
+return shouts;
+}
 
 /**
  * sumArray()
@@ -57,7 +85,24 @@ function shoutForOfLoop(array) {}
  *  sumArray([ 10, 0, 10, 11 ]);
  *  //> 31
  */
-function sumArray(array) {}
+function sumArray(array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
+  // Alt answer using FOR OF loop: 
+  // for (let value of array) {
+  //   sum += value;
+  // }
+  // return sum;
+
+
+// Alt method:
+
+// return array.reduce((prev, curr) => prev, curr, 0);
 
 /**
  * oddArray()
@@ -70,7 +115,19 @@ function sumArray(array) {}
  *  oddArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 11, 15, 37 ]
  */
-function oddArray(array) {}
+function oddArray(array) {
+let numbers = [];
+for (let value of array) {
+  if (value % 2 === 1) {
+    numbers.push(value);
+  }
+}
+return numbers;
+}
+
+// Alt method:
+
+// return array.filter(value => value % 2 === 1);
 
 /**
  * evenArray()
@@ -83,7 +140,19 @@ function oddArray(array) {}
  *  evenArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 20, 22 ]
  */
-function evenArray(array) {}
+function evenArray(array) {
+  let numbers = [];
+  for (let value of array) {
+    if (value % 2 === 0) {
+      numbers.push(value);
+    }
+  }
+  return numbers;
+}
+
+// Alt method: 
+
+// return array.filter(value => value % 2 === 0);
 
 /**
  * findSmallest()
@@ -96,7 +165,19 @@ function evenArray(array) {}
  *  findSmallest([ 0, 11, -2, 5 ]);
  *  //> -2
  */
-function findSmallest(array) {}
+function findSmallest(array) {
+  let smallest = array[0];
+  for (let value of array) {
+    if (value < smallest) {
+      smallest = value;
+    }
+  }
+  return smallest;
+}
+
+// Alt method:
+
+// return Math.min(...array);
 
 /**
  * findLargest()
@@ -109,7 +190,19 @@ function findSmallest(array) {}
  *  findLargest([ 0, 11, -2, 5 ]);
  *  //> 11
  */
-function findLargest(array) {}
+function findLargest(array) {
+  let largest = array[0];
+  for (let value of array) {
+    if (value > largest) {
+      largest = value;
+    }
+   }
+   return largest;
+}
+
+// Alt method:
+
+// return Math.max(...array);
 
 /**
  * findEqual()
@@ -126,7 +219,18 @@ function findLargest(array) {}
  *  findEqual([ 0, 11, -2, 5 ], 9);
  *  //> false
  */
-function findEqual(array, selected) {}
+function findEqual(array, selected) {
+  for (let value of array) {
+    if (value === selected) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Alt method:
+
+// return aray.includes(selected);
 
 /**
  * removeDuplicates()
@@ -143,7 +247,16 @@ function findEqual(array, selected) {}
  *  //> [ 1, 11, 2, 3, 4, 9 ]
  */
 
-function removeDuplicates(array) {}
+function removeDuplicates(array) {
+  let numbers = [];
+
+  for (let value of array) {
+    if (!numbers.includes(value)) {
+      numbers.push(value);
+    }
+  }
+  return numbers;
+}
 
 // Do not change any code below this line.
 module.exports = {
