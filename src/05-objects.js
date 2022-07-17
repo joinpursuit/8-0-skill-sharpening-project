b4c60aa3d2403433a9d5eb386397db20bad2f5b9
jasn4real <tobeobi@pursuit.org>
2022-07-17 13:16:07 -0400

    Tobe Skill Sharpening

--- src/05-objects.js
+++ src/05-objects.js
@@ -9,7 +9,10 @@
  *  createEmptyObject()
  *  //> {}
  */
-function createEmptyObject() {}
+function createEmptyObject() {
+  let emptyObject = {}
+    return emptyObject;
+}
 
 /**
  * createObjectWithValue()
@@ -22,7 +25,11 @@ function createEmptyObject() {}
  *  createObjectWithValue(19);
  *  //> { whateverKey: 19 }
  */
-function createObjectWithValue(val) {}
+function createObjectWithValue(val) {
+  let object = {}
+  object["firstKey"] = val
+  return object;
+}
 
 /**
  * createObjectWithKey()
@@ -35,7 +42,13 @@ function createObjectWithValue(val) {}
  *  createObjectWithKey("left");
  *  //> { left: false }
  */
-function createObjectWithKey(key) {}
+function createObjectWithKey(key) {
+  let value
+  let object = {
+  [key]: value,
+ }
+ return object;
+}
 
 /**
  * createObjectWithKeyValuePair()
@@ -49,7 +62,12 @@ function createObjectWithKey(key) {}
  *  createObjectWithKeyValuePair("left", "right");
  *  //> { left: "right" }
  */
-function createObjectWithKeyValuePair(key, val) {}
+function createObjectWithKeyValuePair(key, val) {
+  let object = {
+    [key]: val,
+   };
+   return object;
+}
 
 /**
  * getNameFromObject()
@@ -62,7 +80,9 @@ function createObjectWithKeyValuePair(key, val) {}
  *  getNameFromObject({ name: "Jay" });
  *  //> "Jay"
  */
-function getNameFromObject(object) {}
+function getNameFromObject(object) {
+  return object.name;
+}
 
 /**
  * getAgeFromObject()
@@ -75,7 +95,9 @@ function getNameFromObject(object) {}
  *  getAgeFromObject({ age: 30 });
  *  //> 30
  */
-function getAgeFromObject(object) {}
+function getAgeFromObject(object) {
+  return object.age;
+}
 
 /**
  * addValueToObject()
@@ -89,7 +111,13 @@ function getAgeFromObject(object) {}
  *  addValueToObject({ left: true }, false);
  *  //> { left: true, key: false }
  */
-function addValueToObject(object, val) {}
+function addValueToObject(object, val) {
+  let key
+  object = {
+    [key]: val,
+  };
+  return object;
+}
 
 /**
  * addKeyToObject()
@@ -103,7 +131,13 @@ function addValueToObject(object, val) {}
  *  addKeyToObject({ right: false }, "left");
  *  //> { right: false, left: true }
  */
-function addKeyToObject(object, key) {}
+function addKeyToObject(object, key) {
+  let value
+  object = {
+  [key]: value,
+ }
+ return object;
+}
 
 /**
  * addKeyValuePairToObject()
@@ -118,7 +152,12 @@ function addKeyToObject(object, key) {}
  *  addKeyValuePairToObject({ up: true }, "left", false);
  *  //> { up: true, left: false }
  */
-function addKeyValuePairToObject(object, key, val) {}
+function addKeyValuePairToObject(object, key, val) {
+  object = {
+    [key]: val,
+  };
+  return object;
+}
 
 /**
  * deleteKeyFromObject()
@@ -132,7 +171,10 @@ function addKeyValuePairToObject(object, key, val) {}
  *  deleteKeyFromObject({ left: true, right: false }, "left");
  *  //> { right: false }
  */
-function deleteKeyFromObject(object, key) {}
+function deleteKeyFromObject(object, key) {
+  delete object[key]
+  return object;
+}
 
 // Do not change any code below this line.
 module.exports = {
