4469c59f8b2deb1132a6f21e9f374b71f3e24033
Isaiah Desrosiers <isaiahdesrosiers21@gmail.com>
2022-10-14 13:41:16 -0400

    Complete the string functions.

--- src/02-data-types.js
+++ src/02-data-types.js
@@ -36,7 +36,9 @@ function convertToNumber(val) {}
  *  convertToString(99);
  *  //> "99"
  */
-function convertToString(val) {}
+function convertToString(val) {
+  return String(val);
+}
 
 /**
  * convertToShoutingText()
@@ -49,7 +51,9 @@ function convertToString(val) {}
  *  convertToShoutingText("Hello There");
  *  //> "HELLO THERE"
  */
-function convertToShoutingText(text) {}
+function convertToShoutingText(text) {
+  return text.toUpperCase();
+}
 
 /**
  * convertToWhisperText()
@@ -62,7 +66,9 @@ function convertToShoutingText(text) {}
  *  convertToWhisperText("Hello There");
  *  //> "hello there"
  */
-function convertToWhisperText(text) {}
+function convertToWhisperText(text) {
+  return text.toLowerCase();
+}
 
 /**
  * checkIfCharacterIsInString()
