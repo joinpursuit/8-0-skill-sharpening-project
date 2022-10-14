c26e89dd9f4d536750634ff97837c016ab13acbb
Shaniqua Coston <shaniquacoston@pursuit.org>
2022-10-14 14:09:54 -0400

    The solutions for fuctions 4-6.

--- src/01-functions.js
+++ src/01-functions.js
@@ -54,7 +54,10 @@ function add() {}
  *  multiply(2, 5);
  *  //> 10
  */
-function multiply() {}
+function multiply(a, b) {
+  let result = a * b;
+  return result;
+}
 
 /**
  * average()
@@ -74,7 +77,10 @@ function multiply() {}
  *  average(10, 6);
  *  //> 8 // 10 + 6 = 16; 16/2 = 8;
  */
-function average() {}
+function average(a, b) {
+  let average = (a + b) /2;
+  return average
+}
 
 /**
  * averageThree()
@@ -93,7 +99,10 @@ function average() {}
  *  averageThree(10, 11, 19);
  *  //> 10 // 10 + 11 + 19 = 30; 30 / 3 = 10;
  */
-function averageThree() {}
+function averageThree(a, b, c) {
+  let average = (a + b + c)/3;
+  return average;
+}
 
 /**
  * compareTypes()
