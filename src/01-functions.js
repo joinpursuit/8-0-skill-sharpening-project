a839f83e6887a9b03ad6b9869bc8f8df33f00d6f
Joshua Nelson <joshuanelsondev@gmail.com>
2022-10-14 13:31:33 -0400

    Complete multiply(), average(), averageThree()

--- src/01-functions.js
+++ src/01-functions.js
@@ -54,7 +54,10 @@ function add() {}
  *  multiply(2, 5);
  *  //> 10
  */
-function multiply() {}
+function multiply(a, b) {
+  
+  return a * b;
+}
 
 /**
  * average()
@@ -74,7 +77,10 @@ function multiply() {}
  *  average(10, 6);
  *  //> 8 // 10 + 6 = 16; 16/2 = 8;
  */
-function average() {}
+function average(a, b) {
+
+  return (a + b) / 2;
+}
 
 /**
  * averageThree()
@@ -93,7 +99,10 @@ function average() {}
  *  averageThree(10, 11, 19);
  *  //> 10 // 10 + 11 + 19 = 30; 30 / 3 = 10;
  */
-function averageThree() {}
+function averageThree(a, b, c) {
+
+  return (a + b + c) / 3;
+} 
 
 /**
  * compareTypes()
