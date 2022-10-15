33ede0ec9ad9254425ef6f0f80f87d8411d969e2
Diandre Miller <millerdiandre@gmail.com>
2022-10-14 21:47:20 -0400

    Diandre final commit for objects

--- src/05-objects.js
+++ src/05-objects.js
@@ -143,7 +143,10 @@ function addKeyValuePairToObject(object, key, val) {}
  *  deleteKeyFromObject({ left: true, right: false }, "left");
  *  //> { right: false }
  */
-function deleteKeyFromObject(object, key) {}
+function deleteKeyFromObject(object, key) {
+  delete object[key]
+  return object
+}
 
 // Do not change any code below this line.
 module.exports = {
