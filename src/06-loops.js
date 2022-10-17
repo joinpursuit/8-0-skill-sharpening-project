828ff646b9e25b1efba5cda6bf1ea2d8c15a0389
Jordan-Walker-Web <Jordan>
2022-10-17 19:31:11 -0400

    Jordan first 4 loops

--- src/06-loops.js
+++ src/06-loops.js
@@ -11,7 +11,7 @@
  *  //> 25
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
-function rangeSum(min, max) {}
+function rangeSum(min, max) { }
 
 /**
  * rangeOdd()
@@ -25,7 +25,7 @@ function rangeSum(min, max) {}
  *  rangeOdd(10, 15);
  *  //> [ 15, 13, 11 ]
  */
-function rangeOdd(min, max) {}
+function rangeOdd(min, max) { }
 
 /**
  * rangeEveryOther()
@@ -39,7 +39,7 @@ function rangeOdd(min, max) {}
  *  rangeEveryOther(11, 18);
  *  //> [ 11, 13, 15, 17 ]
  */
-function rangeEveryOther(min, max) {}
+function rangeEveryOther(min, max) { }
 
 /**
  * containsWhileLoop()
@@ -61,7 +61,7 @@ function rangeEveryOther(min, max) {}
  *  containsWhileLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsWhileLoop(array, target) {}
+function containsWhileLoop(array, target) { }
 
 /**
  * containsForLoop()
@@ -83,7 +83,7 @@ function containsWhileLoop(array, target) {}
  *  containsForLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsForLoop(array, target) {}
+function containsForLoop(array, target) { }
 
 /**
  * targetCount()
@@ -97,7 +97,7 @@ function containsForLoop(array, target) {}
  *  targetCount([ 10, 20, 10, 20, 30 ], 10);
  *  //> 2
  */
-function targetCount(array, target) {}
+function targetCount(array, target) { }
 
 /**
  * firstIndexFound()
@@ -117,7 +117,15 @@ function targetCount(array, target) {}
  *  firstIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function firstIndexFound(array, target) {}
+function firstIndexFound(array, target) { 
+let first = -1
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      return i
+   }
+  }
+  return first ;
+}
 
 /**
  * lastIndexFound()
@@ -137,7 +145,15 @@ function firstIndexFound(array, target) {}
  *  lastIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function lastIndexFound(array, target) {}
+function lastIndexFound(array, target) {
+  let last = -1
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      last = i
+    }
+  }
+  return last;
+}
 
 /**
  * timesIndex()
@@ -151,7 +167,14 @@ function lastIndexFound(array, target) {}
  *  //> [ 7 * 0, 10 * 1, 11 * 2 ]
  *  //> [ 0, 10, 22 ]
  */
-function timesIndex(array) {}
+function timesIndex(array) { 
+  let newArr = []
+  for(let i =0; i < array.length; i++){
+   newArr.push(i * array[i])
+  }
+    return newArr
+
+}
 
 /**
  * cumulativeSum()
@@ -165,7 +188,13 @@ function timesIndex(array) {}
  *  //> [ 5, 5 + 2, 5 + 2 + 9 ]
  *  //> [ 5, 7, 16 ]
  */
-function cumulativeSum(array) {}
+function cumulativeSum(array) { 
+ 
+  for(let i = 1; i < array.length; i++){
+    array[i]= (array[i] + array[i-1])
+  } 
+ return array
+}
 
 // Do not change anything below this line.
 module.exports = {
