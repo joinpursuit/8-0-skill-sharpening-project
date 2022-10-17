eb34e074423a0bd42352df704f60d2a7252d7047
Jyoti Singh <jyo.singh88@gmail.com>
2022-10-17 11:53:04 -0400

    last3ofControlFlow

--- src/03-control-flow.js
+++ src/03-control-flow.js
@@ -151,7 +151,12 @@ function calculateLetterGrade(grade) {}
  *  animalCounts("pig", 1);
  *  //> "1 pig"
  */
-function animalCounts(animal, numberOfAnimals) {}
+function animalCounts(animal, numberOfAnimals) {
+  if(numberOfAnimals > 1){
+    return `${numberOfAnimals} ${animal}s`
+  }
+  return `${numberOfAnimals} ${animal}`;
+}
 
 /**
  * evenOrOdd()
@@ -168,7 +173,15 @@ function animalCounts(animal, numberOfAnimals) {}
  *  evenOrOdd(48);
  *  //> "Even"
  */
-function evenOrOdd(a) {}
+function evenOrOdd(a) {
+  if(a % 2 === 0){
+    return "Even"
+  }
+  // else if (a % 2 !==0){
+    return "Odd"
+  }
+// }
+//dont need the else if//
 
 /**
  * evenOrOddWithTernary()
@@ -184,7 +197,9 @@ function evenOrOdd(a) {}
  *  evenOrOddWithTernary(8);
  *  //> "Even"
  */
-function evenOrOddWithTernary(a) {}
+function evenOrOddWithTernary(a) {
+  return a % 2 === 0? "Even" : "Odd"
+}
 
 // Do not change any code below this line.
 module.exports = {
