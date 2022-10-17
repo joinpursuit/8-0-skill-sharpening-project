5a86fc2688dcc31e65c30f337db66ea5470b258f
Isaiah Desrosiers <isaiahdesrosiers21@gmail.com>
2022-10-17 12:30:41 -0400

    Get number and name.

--- src/08-accessing.js
+++ src/08-accessing.js
@@ -103,7 +103,9 @@ function getFirstPhoneNumber(person) {}
  *  getLastPhoneNumber(person);
  *  //> 7185558611
  */
-function getLastPhoneNumber(person) {}
+function getLastPhoneNumber(person) {
+  return person.numbers[person.numbers.length-1];
+}
 
 /**
  * getFullName()
@@ -116,7 +118,9 @@ function getLastPhoneNumber(person) {}
  *  getFullName(person);
  *  //> "Rachel Eleanor Rojas"
  */
-function getFullName(person) {}
+function getFullName(person) {
+  return `${person.names.first} ${person.names.middle} ${person.names.last}`;
+}
 
 /**
  * getCityAndState()
