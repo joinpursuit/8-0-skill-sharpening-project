9841f41e1bc62b63524e74d3522b8abce8b48900
Markeya Mckoy-Carree <mcmcarree@gmail.com>
2022-10-18 13:35:42 -0400

    Objects 1

--- src/06-loops.js
+++ src/06-loops.js
@@ -11,7 +11,14 @@
  *  //> 25
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
-function rangeSum(min, max) {}
+function rangeSum(min, max) {
+  let sum = 0
+  for (let i = min; i <= max; i ++){
+  sum += i}
+return sum
+  
+
+}
 
 /**
  * rangeOdd()
