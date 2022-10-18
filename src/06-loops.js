c870e7431df355ff33620191671897ed4498279f
Joshua Nelson <joshuanelsondev@gmail.com>
2022-10-18 13:16:42 -0400

    Complete rangeSum(), rangeOdd(), rangeEveryOther()

--- src/06-loops.js
+++ src/06-loops.js
@@ -11,7 +11,13 @@
  *  //> 25
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
-function rangeSum(min, max) {}
+function rangeSum(min, max) {
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+}
 
 /**
  * rangeOdd()
@@ -25,7 +31,16 @@ function rangeSum(min, max) {}
  *  rangeOdd(10, 15);
  *  //> [ 15, 13, 11 ]
  */
-function rangeOdd(min, max) {}
+function rangeOdd(min, max) {
+  let sum = [];
+  for (let i = max; i >= min; i--) {
+    if (i % 2 !== 0){
+      sum.push(i);
+    }
+  } 
+  
+  return sum;
+}
 
 /**
  * rangeEveryOther()
@@ -39,7 +54,14 @@ function rangeOdd(min, max) {}
  *  rangeEveryOther(11, 18);
  *  //> [ 11, 13, 15, 17 ]
  */
-function rangeEveryOther(min, max) {}
+function rangeEveryOther(min, max) {
+  let sum = [];
+  for (let i = min; i <= max; i += 2) {
+    sum.push(i);
+  }
+
+  return sum;
+}
 
 /**
  * containsWhileLoop()
