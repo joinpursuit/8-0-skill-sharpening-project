78391ae7ad46adfaf8d18debafbdb99ec4260148
Ari-Collado <arielacollado@pursuit.org>
2022-10-18 13:46:57 -0400

    done

--- src/09-word-problems.js
+++ src/09-word-problems.js
@@ -19,7 +19,9 @@
  *  applyDiscount(1000, 9, true);
  *  //> 700
  */
-function applyDiscount(priceInCents, age, hasMembership) {}
+function applyDiscount(priceInCents, age, hasMembership) {
+
+}
 
 /**
  * getCartTotal()
@@ -80,7 +82,9 @@ function getCartTotal(products) {}
     compareLocations(address1, address2);
     //> "Same city."
  */
-function compareLocations(address1, address2) {}
+function compareLocations(address1, address2) {
+ 
+}
 
 /**
  * gradeAssignments()
@@ -127,7 +131,32 @@ function compareLocations(address1, address2) {}
     //>   },
     //> ];
  */
-function gradeAssignments(assignments) {}
+function gradeAssignments(assignments) {
+  for (i= 0; i < assignments.length; i++){
+    if (assignments[i].kind == "PASS-FAIL"){
+      if (assignments[i].score.received == assignments[i].score.max){
+        assignments[i].status = "PASSED";
+      } else { 
+        assignments[i].status = "FAILED";
+      }
+    }
+
+    else if (assignments[i].kind == "PERCENTAGE"){
+      percentage = Math.abs((assignments[i].score.received / assignments[i].score.max)*100).toFixed(1);
+      if (percentage >= 80){ 
+        assignments[i].status = `PASSED: ${percentage}%`;
+      } else { 
+        assignments[i].status = `FAILED: ${percentage}%`;
+      }
+    }
+
+    else {
+      assignments[i].status = `SCORE: ${assignments[i].score.received}/${assignments[i].score.max}`;
+    }
+
+    }
+    return assignments;
+}
 
 /**
  * createLineOrder()
@@ -152,7 +181,23 @@ function gradeAssignments(assignments) {}
     createLineOrder(people);
     //> [ "Ray Anderson", "America Marsh", "Wade Carson", "Patience Patel" ]
  */
-function createLineOrder(people) {}
+function createLineOrder(people) {
+  let member = []
+  let nonMember = []
+
+  for (let i = 0; i < people.length; i++) {
+    
+    if (people[i].hasMembership === true) {
+      member.push(people[i].name)
+    }else {
+      nonMember.push(people[i].name)
+    }
+    
+  } 
+people = member.concat(nonMember)
+return people
+}
+
 
 module.exports = {
   applyDiscount,
