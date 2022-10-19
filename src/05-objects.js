06f874f807da16d442cfc3d46403001b2113ad8f
ninajavier <ninajavier13@gmail.com>
2022-10-19 19:21:46 -0400

    05 Objects

--- src/05-objects.js
+++ src/05-objects.js
@@ -9,7 +9,10 @@
  *  createEmptyObject()
  *  //> {}
  */
-function createEmptyObject() {}
+function createEmptyObject() {
+  let obj = {};
+  return obj;
+}
 
 /**
  * createObjectWithValue()
@@ -22,7 +25,12 @@ function createEmptyObject() {}
  *  createObjectWithValue(19);
  *  //> { whateverKey: 19 }
  */
-function createObjectWithValue(val) {}
+function createObjectWithValue(val) {
+  let obj = {
+    whateverKey: val
+  }
+  return obj;
+}
 
 /**
  * createObjectWithKey()
@@ -35,7 +43,11 @@ function createObjectWithValue(val) {}
  *  createObjectWithKey("left");
  *  //> { left: false }
  */
-function createObjectWithKey(key) {}
+function createObjectWithKey(key) {
+  let obj = {};
+  obj[key] = '';
+  return obj;
+}
 
 /**
  * createObjectWithKeyValuePair()
@@ -49,7 +61,11 @@ function createObjectWithKey(key) {}
  *  createObjectWithKeyValuePair("left", "right");
  *  //> { left: "right" }
  */
-function createObjectWithKeyValuePair(key, val) {}
+function createObjectWithKeyValuePair(key, val) {
+  let obj = {};
+  obj[key] = val;
+  return obj;
+}
 
 /**
  * getNameFromObject()
@@ -58,11 +74,13 @@ function createObjectWithKeyValuePair(key, val) {}
  * @param {Object} object
  * @returns {*}
  *
- * EXAMPLE:
+ * EXAMPLE:ZAX
  *  getNameFromObject({ name: "Jay" });
  *  //> "Jay"
  */
-function getNameFromObject(object) {}
+function getNameFromObject(object) {
+  return object.name;
+}
 
 /**
  * getAgeFromObject()
@@ -75,7 +93,9 @@ function getNameFromObject(object) {}
  *  getAgeFromObject({ age: 30 });
  *  //> 30
  */
-function getAgeFromObject(object) {}
+function getAgeFromObject(object) {
+  return object.age;
+}
 
 /**
  * addValueToObject()
@@ -89,7 +109,10 @@ function getAgeFromObject(object) {}
  *  addValueToObject({ left: true }, false);
  *  //> { left: true, key: false }
  */
-function addValueToObject(object, val) {}
+function addValueToObject(object, val) {
+  object.anything = val;
+  return object;
+}
 
 /**
  * addKeyToObject()
@@ -103,7 +126,10 @@ function addValueToObject(object, val) {}
  *  addKeyToObject({ right: false }, "left");
  *  //> { right: false, left: true }
  */
-function addKeyToObject(object, key) {}
+function addKeyToObject(object, key) {
+  object[key] = '';
+  return object;
+}
 
 /**
  * addKeyValuePairToObject()
@@ -118,7 +144,10 @@ function addKeyToObject(object, key) {}
  *  addKeyValuePairToObject({ up: true }, "left", false);
  *  //> { up: true, left: false }
  */
-function addKeyValuePairToObject(object, key, val) {}
+function addKeyValuePairToObject(object, key, val) {
+  object[key] = val;
+  return object;
+}
 
 /**
  * deleteKeyFromObject()
@@ -132,7 +161,10 @@ function addKeyValuePairToObject(object, key, val) {}
  *  deleteKeyFromObject({ left: true, right: false }, "left");
  *  //> { right: false }
  */
-function deleteKeyFromObject(object, key) {}
+function deleteKeyFromObject(object, key) {
+  delete object[key];
+  return object;
+}
 
 // Do not change any code below this line.
 module.exports = {
