d46b468c0836706eec3b38c6c63580feb20fbc5d
SuperNinjaEv <evan0210@gmail.com>
2022-10-19 18:12:51 -0400

    Complete 1-4

--- src/01-functions.js
+++ src/01-functions.js
@@ -9,7 +9,9 @@
  *  alwaysTrue();
  *  //> true
  */
-function alwaysTrue() {}
+function alwaysTrue() {
+  return true
+}
 
 /**
  * greet()
@@ -24,7 +26,9 @@ function alwaysTrue() {}
  *  greet("Ian");
  *  //> "Hello, Ian."
  */
-function greet() {}
+function greet(name) {
+  return `Hello, ${name}.`
+}
 
 /**
  * add()
@@ -39,7 +43,9 @@ function greet() {}
  *  add(10, 20);
  *  //> 30
  */
-function add() {}
+function add(numberA, numberB) {
+  return numberA + numberB
+}
 
 /**
  * multiply()
@@ -54,7 +60,10 @@ function add() {}
  *  multiply(2, 5);
  *  //> 10
  */
-function multiply() {}
+function multiply(numberA, numberB) {
+  let sum = (numberA * numberB)
+  return sum
+}
 
 /**
  * average()
@@ -74,7 +83,11 @@ function multiply() {}
  *  average(10, 6);
  *  //> 8 // 10 + 6 = 16; 16/2 = 8;
  */
-function average() {}
+function average(numberA, numberB) {
+  let sum = numberA + numberB
+  let average = sum / 2
+  return average
+}
 
 /**
  * averageThree()
@@ -93,7 +106,15 @@ function average() {}
  *  averageThree(10, 11, 19);
  *  //> 10 // 10 + 11 + 19 = 30; 30 / 3 = 10;
  */
-function averageThree() {}
+function averageThree(numberA, numberB, numberC) {
+  let sumArray = [numberA, numberB, numberC]
+  let totalNum = sumArray.length
+  let total = 0
+  for (let i = 0; i < totalNum; i++) {
+    total = total + sumArray[i]
+  }
+  return total / totalNum
+}
 
 /**
  * compareTypes()
@@ -112,7 +133,13 @@ function averageThree() {}
  *  compareTypes("left", 10);
  *  //> false
  */
-function compareTypes() {}
+function compareTypes(a, b) {
+  if (typeof (a) === typeof (b)) {
+    return true
+  } else {
+    return false
+  }
+}
 
 /**
  * flipSign()
@@ -131,7 +158,14 @@ function compareTypes() {}
  *  flipSign(50);
  *  //> -50
  */
-function flipSign() {}
+function flipSign(a) {
+  if (a < 0) {
+    a = a * -1;
+  } else {
+    a = -Math.abs(a);
+  }
+  return a
+}
 
 /**
  * joinStringsWithSpaces()
@@ -151,7 +185,9 @@ function flipSign() {}
  *  joinStringsWithSpaces("left", "right", "up", "down", "away");
  *  //> "left right up down away"
  */
-function joinStringsWithSpaces() {}
+function joinStringsWithSpaces(a, b, c, d, e) {
+  return `${a} ${b} ${c} ${d} ${e}`
+}
 
 /**
  * getFirstAndLastCharacter()
@@ -168,7 +204,10 @@ function joinStringsWithSpaces() {}
  *  getFirstAndLastCharacter("upwards");
  *  //> "us"
  */
-function getFirstAndLastCharacter() {}
+function getFirstAndLastCharacter(a) {
+  let length = a.length-1
+  return `${a[0]}${a[length]}`
+}
 
 // Do not change the code below.
 module.exports = {
