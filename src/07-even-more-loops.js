88f8cb7ad96caeb2955d470939af01a3aff9ee82
vg0at <cameronmedina0130@gmail.com>
2022-10-19 14:04:13 -0400

    sumArray(), oddArray(), evenArray()

--- src/07-even-more-loops.js
+++ src/07-even-more-loops.js
@@ -57,7 +57,12 @@ function shoutForOfLoop(array) {}
  *  sumArray([ 10, 0, 10, 11 ]);
  *  //> 31
  */
-function sumArray(array) {}
+function sumArray(array) {
+  let sum = 0;
+  for (let number of array) {
+    sum += number;
+  } return sum;
+}
 
 /**
  * oddArray()
@@ -70,7 +75,13 @@ function sumArray(array) {}
  *  oddArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 11, 15, 37 ]
  */
-function oddArray(array) {}
+function oddArray(array) {
+  let arr = [];
+  for (let number of array) {
+    if (number % 2 === 1)
+    arr.push(number);
+  } return arr;
+}
 
 /**
  * evenArray()
@@ -83,7 +94,13 @@ function oddArray(array) {}
  *  evenArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 20, 22 ]
  */
-function evenArray(array) {}
+function evenArray(array) {
+  let arr = [];
+  for (let number of array) {
+    if (number % 2 === 0)
+    arr.push(number);
+  } return arr;
+}
 
 /**
  * findSmallest()
