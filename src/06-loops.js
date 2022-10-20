b92ff3830160870bad5cfb71c9f471a3358014be
MaxWattan <mcwattanac@gmail.com>
2022-10-19 22:01:39 -0400

    objects finished

--- src/06-loops.js
+++ src/06-loops.js
@@ -11,7 +11,7 @@
  *  //> 25
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
-function rangeSum(min, max) {}
+function rangeSum(min, max) { }
 
 /**
  * rangeOdd()
@@ -25,7 +25,7 @@ function rangeSum(min, max) {}
  *  rangeOdd(10, 15);
  *  //> [ 15, 13, 11 ]
  */
-function rangeOdd(min, max) {}
+function rangeOdd(min, max) { }
 
 /**
  * rangeEveryOther()
@@ -39,7 +39,7 @@ function rangeOdd(min, max) {}
  *  rangeEveryOther(11, 18);
  *  //> [ 11, 13, 15, 17 ]
  */
-function rangeEveryOther(min, max) {}
+function rangeEveryOther(min, max) { }
 
 /**
  * containsWhileLoop()
@@ -61,7 +61,7 @@ function rangeEveryOther(min, max) {}
  *  containsWhileLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsWhileLoop(array, target) {}
+function containsWhileLoop(array, target) { }
 
 /**
  * containsForLoop()
@@ -83,7 +83,14 @@ function containsWhileLoop(array, target) {}
  *  containsForLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsForLoop(array, target) {}
+function containsForLoop(array, target) {
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] === target) {
+      return true
+    }
+  }return false
+} 
+
 
 /**
  * targetCount()
@@ -97,7 +104,7 @@ function containsForLoop(array, target) {}
  *  targetCount([ 10, 20, 10, 20, 30 ], 10);
  *  //> 2
  */
-function targetCount(array, target) {}
+function targetCount(array, target) { }
 
 /**
  * firstIndexFound()
@@ -117,7 +124,7 @@ function targetCount(array, target) {}
  *  firstIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function firstIndexFound(array, target) {}
+function firstIndexFound(array, target) { }
 
 /**
  * lastIndexFound()
@@ -137,7 +144,7 @@ function firstIndexFound(array, target) {}
  *  lastIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function lastIndexFound(array, target) {}
+function lastIndexFound(array, target) { }
 
 /**
  * timesIndex()
@@ -151,7 +158,7 @@ function lastIndexFound(array, target) {}
  *  //> [ 7 * 0, 10 * 1, 11 * 2 ]
  *  //> [ 0, 10, 22 ]
  */
-function timesIndex(array) {}
+function timesIndex(array) { }
 
 /**
  * cumulativeSum()
@@ -165,7 +172,7 @@ function timesIndex(array) {}
  *  //> [ 5, 5 + 2, 5 + 2 + 9 ]
  *  //> [ 5, 7, 16 ]
  */
-function cumulativeSum(array) {}
+function cumulativeSum(array) { }
 
 // Do not change anything below this line.
 module.exports = {
