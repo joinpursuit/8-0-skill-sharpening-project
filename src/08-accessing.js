914280fb909511f45a9e4f79af6ab9dc9aa2313a
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2022-10-20 13:39:20 -0400

    Restore functions deleted after merge.

--- src/08-accessing.js
+++ src/08-accessing.js
@@ -113,7 +113,9 @@ function getFirstPhoneNumber(person) {
  *  getLastPhoneNumber(person);
  *  //> 7185558611
  */
-function getLastPhoneNumber(person) {}
+function getLastPhoneNumber(person) {
+  return person.numbers[person.numbers.length-1]
+}
 
 /**
  * getFullName()
@@ -126,7 +128,9 @@ function getLastPhoneNumber(person) {}
  *  getFullName(person);
  *  //> "Rachel Eleanor Rojas"
  */
-function getFullName(person) {}
+function getFullName(person) {
+  return `${person.name.first} ${person.name.middle} ${person.name.last}`;
+}
 
 /**
  * getCityAndState()
@@ -142,7 +146,9 @@ function getFullName(person) {}
  *  getCityAndState(person);
  *  //> "Staten Island, NY"
  */
-function getCityAndState(person) {}
+function getCityAndState(person) {
+  return `${person.address.city}, ${person.address.state}`;
+}
 
 /**
  * getFullAddress()
