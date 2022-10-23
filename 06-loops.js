932976c9820da2b765afa49d683bd827c45a2c47
改善の火影 <115655242+Kaizennohokage@users.noreply.github.com>
2022-10-22 20:27:37 -0400

    Add files via upload
    <disclaimer:>
    <note: Some files and it's contents are intentionally left blank- we each completed separate parts of the project. Please disregard some duplicate functions, as we worked together up until the last two days. On one of the last two days Wilton was not present and Keridenne and I had to figure things out with our limited knowledge. Keridenne was not present on the last day. As a result, you may not receive 100% conciseness regarding a comparison of my peer's submissions.>

---
+++ 06-loops.js
@@ -0,0 +1,194 @@
+/**
+ * rangeSum()
+ * ---------------------
+ * Takes in range (inclusive) and returns the sum of all numbers in that range.
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  rangeSum(3, 7);
+ *  //> 25
+ *  (3 + 4 + 5 + 6 + 7 = 25)
+ */
+function rangeSum(min, max) {}
+
+/**
+ * rangeOdd()
+ * ---------------------
+ * Takes in range (inclusive) and returns an array in decreasing order of only the odd numbers in that range.
+ * @param {number} min
+ * @param {number} max
+ * @returns {number[]}
+ *
+ * EXAMPLE:
+ *  rangeOdd(10, 15);
+ *  //> [ 15, 13, 11 ]
+ */
+function rangeOdd(min, max) {}
+
+/**
+ * rangeEveryOther()
+ * ---------------------
+ * Takes in range (inclusive) and returns an array in increasing order of only every other element. Start by including the first number.
+ * @param {number} min
+ * @param {number} max
+ * @returns {number[]}
+ *
+ * EXAMPLE:
+ *  rangeEveryOther(11, 18);
+ *  //> [ 11, 13, 15, 17 ]
+ */
+function rangeEveryOther(min, max) {}
+
+/**
+ * containsWhileLoop()
+ * ---------------------
+ * Returns whether or not the `target` value exists in the array.
+ *
+ * NOTE: Must be written with a `while` loop.
+ *
+ * NOTE: Must NOT contain the `.includes()` method.
+ *
+ * @param {*[]} array
+ * @param {number|string} target
+ * @returns {boolean}
+ *
+ * EXAMPLE:
+ *  containsWhileLoop([ 5, 2, 4 ], 4);
+ *  //> true
+ *
+ *  containsWhileLoop([ "left", "up", "right" ], "down");
+ *  //> false
+ */
+function containsWhileLoop(array, target) {
+
+  let i = 0
+  while (i < array.length) {
+   if (array[i] === target) {
+    return true
+   }
+    i++
+  }
+  return false
+}
+
+/**
+ * containsForLoop()
+ * ---------------------
+ * Returns whether or not the `target` value exists in the array.
+ *
+ * NOTE: Must be written with a `for` loop.
+ *
+ * NOTE: Must NOT contain the `.includes()` method.
+ *
+ * @param {*[]} array
+ * @param {number|string} target
+ * @returns {boolean}
+ *
+ * EXAMPLE:
+ *  containsForLoop([ 5, 2, 4 ], 4);
+ *  //> true
+ *
+ *  containsForLoop([ "left", "up", "right" ], "down");
+ *  //> false
+ */
+function containsForLoop(array, target) {}
+
+/**
+ * targetCount()
+ * ---------------------
+ * Returns how many times the `target` occurs in the array.
+ * @param {*[]} array
+ * @param {number|string} target
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  targetCount([ 10, 20, 10, 20, 30 ], 10);
+ *  //> 2
+ */
+function targetCount(array, target) {
+  
+}
+
+/**
+ * firstIndexFound()
+ * ---------------------
+ * Returns the first index that the `target` is found at in the array. If the `target` is not found, it should return `-1`.
+ *
+ * NOTE: You may NOT use the `.indexOf()` method.
+ *
+ * @param {*[]} array
+ * @param {number|string} target
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  firstIndexFound([ "left", "right", "left" ], "left");
+ *  //> 0
+ *
+ *  firstIndexFound([ "left", "right", "left" ], "up");
+ *  //> -1
+ */
+function firstIndexFound(array, target) {}
+
+/**
+ * lastIndexFound()
+ * ---------------------
+ * Returns the last index that the `target` is found at in the array. If the `target` is not found, it should return `-1`.
+ *
+ * NOTE: You may NOT use the `.indexOf()` method.
+ *
+ * @param {*[]} array
+ * @param {number|string} target
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  lastIndexFound([ "left", "right", "left" ], "left");
+ *  //> 2
+ *
+ *  lastIndexFound([ "left", "right", "left" ], "up");
+ *  //> -1
+ */
+function lastIndexFound(array, target) {}
+
+/**
+ * timesIndex()
+ * ---------------------
+ * Returns a new array where each element has been multiplied by its index.
+ * @param {number[]} elements
+ * @returns {number[]}
+ *
+ * EXAMPLE:
+ *  timesIndex([ 7, 10, 11 ]);
+ *  //> [ 7 * 0, 10 * 1, 11 * 2 ]
+ *  //> [ 0, 10, 22 ]
+ */
+function timesIndex(array) {}
+
+/**
+ * cumulativeSum()
+ * ---------------------
+ * Returns a new array where each element is the cumulative sum in the array, up until that point.
+ * @param {number[]} array
+ * @returns {number[]}
+ *
+ * EXAMPLE:
+ *  cumulativeSum([ 5, 2, 9 ]);
+ *  //> [ 5, 5 + 2, 5 + 2 + 9 ]
+ *  //> [ 5, 7, 16 ]
+ */
+function cumulativeSum(array) {}
+
+// Do not change anything below this line.
+module.exports = {
+  rangeSum,
+  rangeOdd,
+  rangeEveryOther,
+  containsWhileLoop,
+  containsForLoop,
+  targetCount,
+  firstIndexFound,
+  lastIndexFound,
+  timesIndex,
+  cumulativeSum,
+};
---
+++ 08-accessing.js
@@ -0,0 +1,207 @@
+/*
+  For all of the functions below, assume each function takes the following `person` object as its input. While the values of the object may change, the structure of it will always be the same.
+
+  const person = {
+    names: {
+      first: "Rachel",
+      middle: "Eleanor",
+      last: "Rojas"
+    },
+    address: {
+      street: "697 Pine Drive",
+      unit: "2A",
+      city: "Staten Island",
+      state: "NY",
+      zip: "10306"
+    },
+    numbers: [
+      7185550921,
+      7185558611
+    ]
+  }
+*/
+
+/**
+ * getFirstName()
+ * ---------------------
+ * Return the `first` name of the inputted person.
+ * @param {Object} person
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  getFirstName(person);
+ *  //> "Rachel"
+ */
+
+
+function getFirstName(person) {
+ return person.names.first
+  }
+  
+
+/**
+ * getLastName()
+ * ---------------------
+ * Return the `last` name of the inputted person.
+ * @param {Object} person
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  getLastName(person);
+ *  //> "Rojas"
+ */
+function getLastName(person) {
+  return person.names.last
+}
+
+/**
+ * getAddressStreet()
+ * ---------------------
+ * Return the `street` of the inputted person's `address`.
+ * @param {Object} person
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  getAddressStreet(person);
+ *  //> "697 Pine Drive"
+ */
+function getAddressStreet(person) {
+  return person.address.street
+}
+
+/**
+ * getCountOfPhoneNumbers()
+ * ---------------------
+ * Return a count of how many phone `numbers` the person has.
+ * @param {Object} person
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  getCountOfPhoneNumbers(person);
+ *  //> 2
+ */
+function getCountOfPhoneNumbers(person) {
+  
+   //I don't know = person.length
+}
+
+
+/**
+ * getFirstPhoneNumber()
+ * ---------------------
+ * Return the first phone number associated with the person.
+ *
+ * NOTE: There will always be at least one phone number.
+ *
+ * @param {Object} person
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  getFirstPhoneNumber(person);
+ *  //> 7185550921
+ */
+function getFirstPhoneNumber(person) {
+  return person.numbers[0]
+}
+
+/**
+ * getLastPhoneNumber()
+ * ---------------------
+ * Return the first phone number associated with the person.
+ *
+ * NOTE: There will always be at least one phone number.
+ *
+ * @param {Object} person
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  getLastPhoneNumber(person);
+ *  //> 7185558611
+ */
+function getLastPhoneNumber(person) {
+  return person.numbers[person.numbers.length -1]
+}
+
+/**
+ * getFullName()
+ * ---------------------
+ * Combine the `first`, `middle`, and `last` names to return the full name.
+ * @param {Object} person
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  getFullName(person);
+ *  //> "Rachel Eleanor Rojas"
+ */
+function getFullName(person) {}
+
+/**
+ * getCityAndState()
+ * ---------------------
+ * Combine the `city` and `state` of the person's `address`.
+ *
+ * NOTE: Pay attention to the formatting below.
+ *
+ * @param {Object} person
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  getCityAndState(person);
+ *  //> "Staten Island, NY"
+ */
+function getCityAndState(person) {}
+
+/**
+ * getFullAddress()
+ * ---------------------
+ * Combine all of the `address` fields to create a full address for the person.
+ *
+ * NOTE: Pay attention to the formatting below.
+ *
+ * @param {Object} person
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  getFullAddress(person);
+ *  //> "697 Pine Drive 2A, Staten Island, NY, 10306"
+ */
+function getFullAddress(person) {}
+
+/**
+ * getFlatObject()
+ * ---------------------
+ * Create a new object where all of the keys from the `names` and `address` objects are in the same object. You may leave the `numbers` array as is.
+ * @param {Object} person 
+ * @returns {Object}
+ * 
+ * EXAMPLE:
+ *  getFlatObject(person);
+ *  //> {
+      first: "Rachel",
+      middle: "Eleanor",
+      last: "Rojas",
+      street: "697 Pine Drive",
+      unit: "2A",
+      city: "Staten Island",
+      state: "NY",
+      zip: "10306",
+      numbers: [7185550921, 7185558611],
+    };
+ */
+function getFlatObject(person) {
+  
+}
+
+// Do not change the code below.
+module.exports = {
+  getFirstName,
+  getLastName,
+  getAddressStreet,
+  getCountOfPhoneNumbers,
+  getFirstPhoneNumber,
+  getLastPhoneNumber,
+  getFullName,
+  getCityAndState,
+  getFullAddress,
+  getFlatObject,
+};
