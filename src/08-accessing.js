f1679814a1f248709d64495cbb8fb795e5b6a65b
Jihan Lewis <jihanlewis@Jihans-MacBook-Air.local>
2022-10-23 14:00:02 -0400

    please be kind

--- src/08-accessing.js
+++ src/08-accessing.js
@@ -32,7 +32,14 @@
  *  getFirstName(person);
  *  //> "Rachel"
  */
-function getFirstName(person) {}
+function getFirstName(person) {
+  let names = {
+    first: "Rachel",
+    middle: "Eleanor",
+    last: "Rojas"
+  }
+  return (person.names.first)
+}
 
 /**
  * getLastName()
@@ -45,7 +52,9 @@ function getFirstName(person) {}
  *  getLastName(person);
  *  //> "Rojas"
  */
-function getLastName(person) {}
+function getLastName(person) {
+  return (person.names.last)
+}
 
 /**
  * getAddressStreet()
@@ -58,7 +67,9 @@ function getLastName(person) {}
  *  getAddressStreet(person);
  *  //> "697 Pine Drive"
  */
-function getAddressStreet(person) {}
+function getAddressStreet(person) {
+  return (person.address.street)
+}
 
 /**
  * getCountOfPhoneNumbers()
@@ -71,7 +82,10 @@ function getAddressStreet(person) {}
  *  getCountOfPhoneNumbers(person);
  *  //> 2
  */
-function getCountOfPhoneNumbers(person) {}
+function getCountOfPhoneNumbers(person) {
+  //let numbers = numbers.length
+  //return (person.numbers...
+}
 
 /**
  * getFirstPhoneNumber()
@@ -87,7 +101,9 @@ function getCountOfPhoneNumbers(person) {}
  *  getFirstPhoneNumber(person);
  *  //> 7185550921
  */
-function getFirstPhoneNumber(person) {}
+function getFirstPhoneNumber(person) {
+  return (person.numbers[0])
+}
 
 /**
  * getLastPhoneNumber()
@@ -103,7 +119,9 @@ function getFirstPhoneNumber(person) {}
  *  getLastPhoneNumber(person);
  *  //> 7185558611
  */
-function getLastPhoneNumber(person) {}
+function getLastPhoneNumber(person) {
+  return (person.numbers[1])
+}
 
 /**
  * getFullName()
@@ -116,7 +134,9 @@ function getLastPhoneNumber(person) {}
  *  getFullName(person);
  *  //> "Rachel Eleanor Rojas"
  */
-function getFullName(person) {}
+function getFullName(person) {
+  return ((person.names.first) + " " + person.names.middle + " " + (person.names.last));
+}
 
 /**
  * getCityAndState()
@@ -132,7 +152,9 @@ function getFullName(person) {}
  *  getCityAndState(person);
  *  //> "Staten Island, NY"
  */
-function getCityAndState(person) {}
+function getCityAndState(person) {
+  return ((person.address.city) + "," + " " + (person.address.state));
+}
 
 /**
  * getFullAddress()
@@ -148,7 +170,9 @@ function getCityAndState(person) {}
  *  getFullAddress(person);
  *  //> "697 Pine Drive 2A, Staten Island, NY, 10306"
  */
-function getFullAddress(person) {}
+function getFullAddress(person) {
+  return ((person.address.street) + " " + (person.address.unit) + "," + " " + (person.address.city) + "," + " " + (person.address.state) + "," + " " + (person.address.zip))
+}
 
 /**
  * getFlatObject()
@@ -171,7 +195,9 @@ function getFullAddress(person) {}
       numbers: [7185550921, 7185558611],
     };
  */
-function getFlatObject(person) {}
+function getFlatObject(person) {
+  return (person.create(names) + person.create(address))
+}
 
 // Do not change the code below.
 module.exports = {
