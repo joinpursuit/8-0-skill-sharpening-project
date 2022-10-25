7094bf56f9f21eb1122aefeff9e0f9360a27d3f3
Colin Jaffe <balloonasaurus@gmail.com>
2022-10-25 06:48:19 -0400

    Fixes indentation.

--- src/02-data-types.js
+++ src/02-data-types.js
@@ -9,166 +9,166 @@
  *  getLengthOfString("down");
  *  //> 4
  */
- function getLengthOfString(str) {}
+function getLengthOfString(str) {}
 
- /**
-  * convertToNumber()
-  * ---------------------
-  * Converts the inputted value into a number.
-  * @param {*} val
-  * @returns {number}
-  *
-  * EXAMPLE:
-  *  convertToNumber("111");
-  *  //> 111
-  */
- function convertToNumber(val) {}
- 
- /**
-  * convertToString()
-  * ---------------------
-  * Converts the inputted value into a string.
-  * The value is guaranteed to NOT be null or undefined.
-  * @param {*} val
-  * @returns {string}
-  *
-  * EXAMPLE:
-  *  convertToString(99);
-  *  //> "99"
-  */
- function convertToString(val) {
-   
- }
- 
- /**
-  * convertToShoutingText()
-  * ---------------------
-  * Converts the inputted text into all uppercase characters.
-  * @param {string} text
-  * @returns {string}
-  *
-  * EXAMPLE:
-  *  convertToShoutingText("Hello There");
-  *  //> "HELLO THERE"
-  */
- function convertToShoutingText(text) {}
- /**
-  * convertToWhisperText()
-  * ---------------------
-  * Converts the inputted text into all lowercase characters.
-  * @param {string} text
-  * @returns {string}
-  *
-  * EXAMPLE:
-  *  convertToWhisperText("Hello There");
-  *  //> "hello there"
-  */
- function convertToWhisperText(text) {}
- 
- /**
-  * checkIfCharacterIsInString()
-  * ---------------------
-  * Checks if the given `character` appears in the `text`.
-  * @param {string} text
-  * @param {string} character
-  * @returns {boolean}
-  *
-  * EXAMPLE:
-  *  checkIfCharacterIsInString("hello there", "e");
-  *  //> true
-  *
-  *  checkIfCharacterIsInString("hello there", "a");
-  *  //> false
-  */
- function checkIfCharacterIsInString(text, character) {}
- 
- /**
-  * isEven()
-  * ---------------------
-  * Returns `true` if the number given is even. Otherwise, returns false.
-  * @param {number} num
-  * @returns {boolean}
-  *
-  * EXAMPLE:
-  *  isEven(10);
-  *  //> true
-  *
-  *  isEven(11);
-  *  //> false
-  */
- function isEven(num) {
-   if (num % 2 === 0) {
-     return true
-   } else{
-     return false
- }
- }
- /**
-  * isOdd()
-  * ---------------------
-  * Returns `true` if the number given is odd. Otherwise, returns `false`.
-  * @param {number} num
-  * @returns {boolean}
-  *
-  * EXAMPLE:
-  *  isOdd(10);
-  *  //> false
-  *
-  *  isOdd(11);
-  *  //> true
-  */
- function isOdd(num) {
-   if (num % 2 === 1) {
-     return true
-   }else{
-     return false
-   }
- }
- 
- /**
-  * isTruthy()
-  * ---------------------
-  * Returns `true` if the value given is a truthy value. Otherwise, returns `false`.
-  * @param {*} val
-  * @returns {boolean}
-  *
-  * EXAMPLE:
-  *  isTruthy(10);
-  *  //> true
-  *
-  *  isTruthy(null);
-  *  //> false
-  */
- function isTruthy(val) {
-   if (val){
-     return true
-   } else{
-       return false
-   }
- }
- 
- /**
-  * isFalsy()
-  * ---------------------
-  * Returns `true` if the value given is a falsy value. Otherwise, returns `false`.
-  * @param {*} val
-  * @returns {boolean}
-  *
-  * EXAMPLE:
-  *  isFalsy(undefined);
-  *  //> true
-  *
-  *  isFalsy(" ");
-  *  //> false
-  */
- function isFalsy(val) {
-   if (!val){
-     return true
-   } else{
-     return false
-   }
- }
- 
+/**
+ * convertToNumber()
+ * ---------------------
+ * Converts the inputted value into a number.
+ * @param {*} val
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  convertToNumber("111");
+ *  //> 111
+ */
+function convertToNumber(val) {}
+
+/**
+ * convertToString()
+ * ---------------------
+ * Converts the inputted value into a string.
+ * The value is guaranteed to NOT be null or undefined.
+ * @param {*} val
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  convertToString(99);
+ *  //> "99"
+ */
+function convertToString(val) {
+
+}
+
+/**
+ * convertToShoutingText()
+ * ---------------------
+ * Converts the inputted text into all uppercase characters.
+ * @param {string} text
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  convertToShoutingText("Hello There");
+ *  //> "HELLO THERE"
+ */
+function convertToShoutingText(text) {}
+/**
+ * convertToWhisperText()
+ * ---------------------
+ * Converts the inputted text into all lowercase characters.
+ * @param {string} text
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  convertToWhisperText("Hello There");
+ *  //> "hello there"
+ */
+function convertToWhisperText(text) {}
+
+/**
+ * checkIfCharacterIsInString()
+ * ---------------------
+ * Checks if the given `character` appears in the `text`.
+ * @param {string} text
+ * @param {string} character
+ * @returns {boolean}
+ *
+ * EXAMPLE:
+ *  checkIfCharacterIsInString("hello there", "e");
+ *  //> true
+ *
+ *  checkIfCharacterIsInString("hello there", "a");
+ *  //> false
+ */
+function checkIfCharacterIsInString(text, character) {}
+
+/**
+ * isEven()
+ * ---------------------
+ * Returns `true` if the number given is even. Otherwise, returns false.
+ * @param {number} num
+ * @returns {boolean}
+ *
+ * EXAMPLE:
+ *  isEven(10);
+ *  //> true
+ *
+ *  isEven(11);
+ *  //> false
+ */
+function isEven(num) {
+  if (num % 2 === 0) {
+    return true
+  } else{
+    return false
+}
+}
+/**
+ * isOdd()
+ * ---------------------
+ * Returns `true` if the number given is odd. Otherwise, returns `false`.
+ * @param {number} num
+ * @returns {boolean}
+ *
+ * EXAMPLE:
+ *  isOdd(10);
+ *  //> false
+ *
+ *  isOdd(11);
+ *  //> true
+ */
+function isOdd(num) {
+  if (num % 2 === 1) {
+    return true
+  }else{
+    return false
+  }
+}
+
+/**
+ * isTruthy()
+ * ---------------------
+ * Returns `true` if the value given is a truthy value. Otherwise, returns `false`.
+ * @param {*} val
+ * @returns {boolean}
+ *
+ * EXAMPLE:
+ *  isTruthy(10);
+ *  //> true
+ *
+ *  isTruthy(null);
+ *  //> false
+ */
+function isTruthy(val) {
+  if (val){
+    return true
+  } else{
+      return false
+  }
+}
+
+/**
+ * isFalsy()
+ * ---------------------
+ * Returns `true` if the value given is a falsy value. Otherwise, returns `false`.
+ * @param {*} val
+ * @returns {boolean}
+ *
+ * EXAMPLE:
+ *  isFalsy(undefined);
+ *  //> true
+ *
+ *  isFalsy(" ");
+ *  //> false
+ */
+function isFalsy(val) {
+  if (!val){
+    return true
+  } else{
+    return false
+  }
+}
+
  // Do not change the code below.
  module.exports = {
    getLengthOfString,
--- src/03-control-flow.js
+++ src/03-control-flow.js
@@ -14,210 +14,210 @@
  *  isEqual(10, "10");
  *  //> false
  */
- function isEqual(a, b) {}
+function isEqual(a, b) {}
 
- /**
-  * findLarger()
-  * ---------------------
-  * Takes in two numbers and returns the larger number. The numbers are guaranteed to be unique (not equal to each other).
-  * @param {number} a
-  * @param {number} b
-  * @returns {number}
-  *
-  * EXAMPLE:
-  *  findLarger(19, 7);
-  *  //> 19
-  */
- function findLarger(a, b) {
+/**
+ * findLarger()
+ * ---------------------
+ * Takes in two numbers and returns the larger number. The numbers are guaranteed to be unique (not equal to each other).
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ *
+ * EXAMPLE:
+ *  findLarger(19, 7);
+ *  //> 19
+ */
+function findLarger(a, b) {
+
+}
+
+/**
+ * findLargerOrTie()
+ * ---------------------
+ * Takes in two numbers and returns the larger number. Returns the string "tie" if both numbers are the same.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number|string}
+ *
+ * EXAMPLE:
+ *  findLargerOrTie(0, 1);
+ *  //> 1
+ *
+ *  findLargerOrTie(0, 0);
+ *  //> "tie"
+ */
+function findLargerOrTie(a, b) {}
+
+/**
+ * positiveNegativeOrZero()
+ * ---------------------
+ * Takes in a number and returns whether that number is "Positive", "Negative", or "Zero".
+ * HINT: Capitalization and spelling count!
+ * @param {number} a
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  positiveNegativeOrZero(-1);
+ *  //> "Negative"
+ *
+ *  positiveNegativeOrZero(0);
+ *  //> "Zero"
+ *
+ *  positiveNegativeOrZero(1);
+ *  //> "Positive"
+ */
+function positiveNegativeOrZero(a) {}
+
+/**
+ * aroundTheWorldGreeting()
+ * ---------------------
+ * Takes in a `language` (e.g "english", "spanish", or "german") and returns "Hello World" in that language. Must work for at least "english", "spanish", and "german". It should default to English if no argument is passed in.
+ *
+ * TRANSLATIONS: english -> "Hello World"
+ *               spanish -> "Hola Mundo"
+ *               german  -> "Hallo Welt"
+ * HINT: The function expects lowercase names of languages.
+ * @param {string} language
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  aroundTheWorldGreeting("spanish");
+ *  //> "Hola Mundo"
+ *
+ *  aroundTheWorldGreeting();
+ *  //> "Hello World"
+ */
+function aroundTheWorldGreeting(language) {}
+
  
- }
- 
- /**
-  * findLargerOrTie()
-  * ---------------------
-  * Takes in two numbers and returns the larger number. Returns the string "tie" if both numbers are the same.
-  * @param {number} a
-  * @param {number} b
-  * @returns {number|string}
-  *
-  * EXAMPLE:
-  *  findLargerOrTie(0, 1);
-  *  //> 1
-  *
-  *  findLargerOrTie(0, 0);
-  *  //> "tie"
-  */
- function findLargerOrTie(a, b) {}
- 
- /**
-  * positiveNegativeOrZero()
-  * ---------------------
-  * Takes in a number and returns whether that number is "Positive", "Negative", or "Zero".
-  * HINT: Capitalization and spelling count!
-  * @param {number} a
-  * @returns {string}
-  *
-  * EXAMPLE:
-  *  positiveNegativeOrZero(-1);
-  *  //> "Negative"
-  *
-  *  positiveNegativeOrZero(0);
-  *  //> "Zero"
-  *
-  *  positiveNegativeOrZero(1);
-  *  //> "Positive"
-  */
- function positiveNegativeOrZero(a) {}
- 
- /**
-  * aroundTheWorldGreeting()
-  * ---------------------
-  * Takes in a `language` (e.g "english", "spanish", or "german") and returns "Hello World" in that language. Must work for at least "english", "spanish", and "german". It should default to English if no argument is passed in.
-  *
-  * TRANSLATIONS: english -> "Hello World"
-  *               spanish -> "Hola Mundo"
-  *               german  -> "Hallo Welt"
-  * HINT: The function expects lowercase names of languages.
-  * @param {string} language
-  * @returns {string}
-  *
-  * EXAMPLE:
-  *  aroundTheWorldGreeting("spanish");
-  *  //> "Hola Mundo"
-  *
-  *  aroundTheWorldGreeting();
-  *  //> "Hello World"
-  */
- function aroundTheWorldGreeting(language) {}
- 
- 
- /**
-  * aroundTheWorldGreetingWithSwitch()
-  * ---------------------
-  * Takes in a language (e.g "english", "spanish", "german") and returns "Hello World" in that language. Must work for at least "english", "spanish", and "german". It should default to English if no argument is passed in.
-  *
-  * TRANSLATIONS: English -> "Hello World"
-  *               Spanish -> "Hola Mundo"
-  *               German  -> "Hallo Welt"
-  *
-  * NOTE: You must use a `switch` statement in solving this.
-  *
-  * @param {string} language
-  * @returns {string}
-  *
-  * EXAMPLE:
-  *  aroundTheWorldGreetingWithSwitch("german");
-  *  //> "Hallo Welt"
-  *
-  *  aroundTheWorldGreetingWithSwitch();
-  *  //> "Hello World"
-  */
- function aroundTheWorldGreetingWithSwitch(language) {
-   switch (language) {
-     case 'english':
-       return "Hello World"
-     case 'spanish':
-       return "Hola Mundo"
-     case 'german':
-       return "Hallo Welt"
-     default: 
-       return 'Hello World'
-   }
- }
- 
- /**
-  * calculateLetterGrade()
-  * ---------------------
-  * Takes in a number and returns the letter grade corresponding to that number. Letter grades consist of "A", "B", "C", "D", or "F".
-  *
-  * SCORING: 90 and above is an A
-  *          80 and above is a B
-  *          70 and above is a C
-  *          60 and above is a D
-  *          Anything lower than 60 is an F
-  * @param {number} grade
-  * @returns {string}
-  *
-  * EXAMPLE:
-  *  calculateLetterGrade(99);
-  *  //> "A"
-  */
- function calculateLetterGrade(grade) {
-   if (grade >= 90) {
-     return "A"
-   }else if ( grade >= 80){
-     return "B"
-   } else if (grade >= 70) {
- 
-     return "C"
-   } else if (grade >= 60) {
-     return "D"
-   } else {
-     return "F"
-   }
- 
-   }
- 
- /**
-  * animalCounts()
-  * ---------------------
-  * Returns the count of animals, properly pluralizing the animal if there is more than one.
-  * The animal is guaranteed to be one you can pluralize by adding `s`. (NO geese, fish, deer, oxen, etc.)
-  * @param {string} animal
-  * @param {number} numberOfAnimals
-  * @returns {string}
-  *
-  * EXAMPLE:
-  *  animalCounts("dog", 2);
-  *  //> "2 dogs"
-  *
-  *  animalCounts("pig", 1);
-  *  //> "1 pig"
-  */
- function animalCounts(animal, numberOfAnimals) {
- 
- if (numberOfAnimals === 1) {
-   return `1 ${animal}`
- } else if (numberOfAnimals >= 2) {
-   return `${numberOfAnimals} ${animal}` + "s"
- 
- }
- 
- }
- 
- /**
-  * evenOrOdd()
-  * ---------------------
-  * Takes in a number and returns whether that number is "Even" or "Odd".
-  * HINT: How is this similar to isOdd() or isEven() from part 2?
-  * @param {number} a
-  * @returns {string}
-  *
-  * EXAMPLE:
-  *  evenOrOdd(11);
-  *  //> "Odd"
-  * 
-  *  evenOrOdd(48);
-  *  //> "Even"
-  */
- function evenOrOdd(a) {
- }
- /**
-  * evenOrOddWithTernary()
-  * ---------------------
-  * Takes in a number and returns whether that number is even or odd.
-  *
-  * NOTE: You must use a ternary to solve this.
-  *
-  * @param {number} a
-  * @returns {string}
-  *
-  * EXAMPLE:
-  *  evenOrOddWithTernary(8);
-  *  //> "Even"
-  */
- function evenOrOddWithTernary(a) {} 
- 
+/**
+ * aroundTheWorldGreetingWithSwitch()
+ * ---------------------
+ * Takes in a language (e.g "english", "spanish", "german") and returns "Hello World" in that language. Must work for at least "english", "spanish", and "german". It should default to English if no argument is passed in.
+ *
+ * TRANSLATIONS: English -> "Hello World"
+ *               Spanish -> "Hola Mundo"
+ *               German  -> "Hallo Welt"
+ *
+ * NOTE: You must use a `switch` statement in solving this.
+ *
+ * @param {string} language
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  aroundTheWorldGreetingWithSwitch("german");
+ *  //> "Hallo Welt"
+ *
+ *  aroundTheWorldGreetingWithSwitch();
+ *  //> "Hello World"
+ */
+function aroundTheWorldGreetingWithSwitch(language) {
+  switch (language) {
+    case 'english':
+      return "Hello World"
+    case 'spanish':
+      return "Hola Mundo"
+    case 'german':
+      return "Hallo Welt"
+    default:
+      return 'Hello World'
+  }
+}
+
+/**
+ * calculateLetterGrade()
+ * ---------------------
+ * Takes in a number and returns the letter grade corresponding to that number. Letter grades consist of "A", "B", "C", "D", or "F".
+ *
+ * SCORING: 90 and above is an A
+ *          80 and above is a B
+ *          70 and above is a C
+ *          60 and above is a D
+ *          Anything lower than 60 is an F
+ * @param {number} grade
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  calculateLetterGrade(99);
+ *  //> "A"
+ */
+function calculateLetterGrade(grade) {
+  if (grade >= 90) {
+    return "A"
+  }else if ( grade >= 80){
+    return "B"
+  } else if (grade >= 70) {
+
+    return "C"
+  } else if (grade >= 60) {
+    return "D"
+  } else {
+    return "F"
+  }
+
+  }
+
+/**
+ * animalCounts()
+ * ---------------------
+ * Returns the count of animals, properly pluralizing the animal if there is more than one.
+ * The animal is guaranteed to be one you can pluralize by adding `s`. (NO geese, fish, deer, oxen, etc.)
+ * @param {string} animal
+ * @param {number} numberOfAnimals
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  animalCounts("dog", 2);
+ *  //> "2 dogs"
+ *
+ *  animalCounts("pig", 1);
+ *  //> "1 pig"
+ */
+function animalCounts(animal, numberOfAnimals) {
+
+if (numberOfAnimals === 1) {
+  return `1 ${animal}`
+} else if (numberOfAnimals >= 2) {
+  return `${numberOfAnimals} ${animal}` + "s"
+
+}
+
+}
+
+/**
+ * evenOrOdd()
+ * ---------------------
+ * Takes in a number and returns whether that number is "Even" or "Odd".
+ * HINT: How is this similar to isOdd() or isEven() from part 2?
+ * @param {number} a
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  evenOrOdd(11);
+ *  //> "Odd"
+ * 
+ *  evenOrOdd(48);
+ *  //> "Even"
+ */
+function evenOrOdd(a) {
+}
+/**
+ * evenOrOddWithTernary()
+ * ---------------------
+ * Takes in a number and returns whether that number is even or odd.
+ *
+ * NOTE: You must use a ternary to solve this.
+ *
+ * @param {number} a
+ * @returns {string}
+ *
+ * EXAMPLE:
+ *  evenOrOddWithTernary(8);
+ *  //> "Even"
+ */
+function evenOrOddWithTernary(a) {}
+
  // Do not change any code below this line.
  module.exports = {
    isEqual,
